b7b91195ffa22d6f146888838336fbac63e92fff
Jared Allard <jared.allard@outreach.io>
2021-04-21 09:44:27 -0600

    fix(shellcheck): properly call get_repo_directory

--- shell/shellcheck.sh
+++ shell/shellcheck.sh
@@ -3,20 +3,18 @@
 # Useful for using the correct version of shellcheck
 # with your editor.
 
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
 # shellcheck source=./lib/bootstrap.sh
 source "$SCRIPTS_DIR/lib/bootstrap.sh"
+# shellcheck source=./lib/logging.sh
+source "$SCRIPTS_DIR/lib/logging.sh"
 
-SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 BIN_DIR="$(get_repo_directory)/bin"
-
 SHELLCHECK_VERSION="$(get_application_version "shellcheck")"
-
 GOOS=$(go env GOOS)
 ARCH=$(uname -m)
 binPath="$BIN_DIR/shellcheck-$SHELLCHECK_VERSION"
-
-# shellcheck source=./lib/logging.sh
-source "$SCRIPTS_DIR/lib/logging.sh"
 
 # Ensure $BIN_DIR exists, since GOBIN makes it, but
 # we may run before it.
