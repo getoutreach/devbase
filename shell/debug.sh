a5db43523b8ea97c32227c56218bd33dbf86644e
Mark Lee <mark.lee@outreach.io>
2021-05-13 09:55:36 -0700

    feat: add debug script from bootstrap

---
+++ shell/debug.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+# shellcheck source=./lib/bootstrap.sh
+source "$SCRIPTS_DIR/lib/bootstrap.sh"
+
+export GOPROXY=https://proxy.golang.org
+export GOPRIVATE="github.com/getoutreach/*"
+export CGO_ENABLED=1
+
+set -ex
+
+exec "$SCRIPTS_DIR/gobin.sh" github.com/go-delve/delve/cmd/dlv@v"$(get_application_version "delve")" debug --build-flags="-tags=or_dev" "$(get_repo_directory)/cmd/$(get_app_name)"
