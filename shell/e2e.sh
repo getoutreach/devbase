2f6daaf274f519f6ba8eb8fc793aa3253ced6e5c
Jared Allard <jared.allard@outreach.io>
2021-06-11 14:00:20 -0700

    fix: install vault JIT

--- shell/e2e.sh
+++ shell/e2e.sh
@@ -6,6 +6,11 @@ if [[ $CI == "true" ]]; then
   "$DIR/circleci/setup-vault.sh"
   "$DIR/circleci/setup-e2e.sh"
 
+  # Install some dependencies
+  curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+  sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+  sudo apt-get update -y && sudo apt-get install -y vault
+
   # Bootstrap puts this here. We could def make this better.
   # sudo is used here because CI has to do some docker perm hacks
   sudo docker exec -it devenv /host_mnt/scripts/shell-wrapper.sh gobin.sh "github.com/getoutreach/devbase/e2e@$(cat "$DIR/../.version")"
