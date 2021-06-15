23c92e4a006d449b3fa51f01866ad4872318ef4f
Jared Allard <jared.allard@outreach.io>
2021-06-15 12:35:46 -0700

    refactor: use apt-get over apt for better output in CI

--- shell/circleci/setup-e2e-deps.sh
+++ shell/circleci/setup-e2e-deps.sh
@@ -4,5 +4,5 @@
 # Install some dependencies
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt update
-sudo apt install -y vault
+sudo apt-get update
+sudo apt-get install -y vault
