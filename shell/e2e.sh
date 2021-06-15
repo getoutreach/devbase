f9201ec112a5c609eae5cc055d0034c993dadd8d
Jared Allard <jared.allard@outreach.io>
2021-06-15 12:30:30 -0700

    feat: seperate E2E deps

--- shell/e2e.sh
+++ shell/e2e.sh
@@ -3,19 +3,14 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 if [[ $CI == "true" ]]; then
-  # Install some dependencies
-  curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-  sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-  sudo apt update
-  sudo apt install -y vault
-
+  "$DIR/circleci/setup-e2e-deps.sh"
   "$DIR/circleci/setup-vault.sh"
   "$DIR/circleci/setup-e2e.sh"
 
   # Bootstrap puts this here. We could def make this better.
   # sudo is used here because CI has to do some docker perm hacks
-  sudo docker exec -it --user circleci -w /host_mnt -e CI=true -e "VAULT_ADDR=$VAULT_ADDR" devenv \
-    ./scripts/shell-wrapper.sh gobin.sh "github.com/getoutreach/devbase/e2e@$(cat "$DIR/../.version")"
+  sudo docker exec --user circleci -w /host_mnt -e CI=true -it -e "VAULT_ADDR=$VAULT_ADDR" devenv bash \
+    -c "source .bootstrap/shell/lib/ssh-auth.sh; ./scripts/shell-wrapper.sh gobin.sh 'github.com/getoutreach/devbase/e2e@$(cat $DIR/../.version)'"
 else
   exec $("$DIR/gobin.sh" -p "github.com/getoutreach/devbase/e2e@$(cat "$DIR/../.version")")
 fi
