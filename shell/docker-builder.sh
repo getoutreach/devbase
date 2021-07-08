9bb938440ec4fc8cc2583d1f0fc59b3b3b81a262
Nissim Natanov <nissim.natanov@outreach.io>
2021-07-08 14:30:39 -0700

    merge Jared's PR

--- shell/docker-builder.sh
+++ shell/docker-builder.sh
@@ -24,27 +24,23 @@ source "${LIB_DIR}/buildx.sh"
 # shellcheck source=./lib/ssh-auth.sh
 source "${LIB_DIR}/ssh-auth.sh"
 
-extraArgs=("-t" "$remote_image_name:${VERSION}")
-if [[ -n ${CIRCLE_TAG} ]]; then
-  # Only push on a tag
-  extraArgs+=("--push")
-else
-  echo "Note: Skipping twist-scan due to buildx limitations"
-  # TODO: buildx doesn't currently support this.
-  # Load it into the docker cache so we can run twist-scan
-  #extraArgs+=("--load")
-fi
+args=("--ssh" "default" "--progress=plain" "--file" "deployments/${appName}/Dockerfile" "--build-arg" "VERSION=${VERSION}")
 
-echo "🔨 Building Docker Image"
-set -x
-docker buildx build --ssh default --progress=plain \
-  --platform linux/arm64,linux/amd64 \
-  --file "deployments/${appName}/Dockerfile" \
-  --build-arg "VERSION=${VERSION}" "${extraArgs[@]}" .
-set +x
+# Build a quick native image on PRs and load it into docker cache
+# for security scanning
+# TODO: before final merge: uncomment if/fi
+#if [[ -z $CIRCLE_TAG ]]; then
+  info "Building Docker Image (test)"
+  docker buildx build "${args[@]}" -t "${appName}" --load .
 
-if [[ -z ${CIRCLE_TAG} ]]; then
-  # Scan the built image
-  info "Scanning docker image for vulnerabilities"
+  info "🔐 Scanning docker image for vulnerabilities"
   source "${TWIST_SCAN_DIR}/twist-scan.sh" "${appName}"
+#fi
+
+if [[ -n ${CIRCLE_TAG} ]]; then
+  echo "🔨 Building and Pushing Docker Image (production)"
+  set -x
+  docker buildx build "${args[@]}" --platform linux/arm64,linux/amd64 \
+    -t "${remote_image_name}:${VERSION}" --push .
+  set +x
 fi
