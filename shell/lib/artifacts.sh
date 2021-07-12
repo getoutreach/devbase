58a85e258d90c02214a6507aef2621a5e575c63a
Nissim Natanov <nissim.natanov@outreach.io>
2021-07-12 16:12:05 -0700

    switch to v1.1 to download artifacts

--- shell/lib/artifacts.sh
+++ shell/lib/artifacts.sh
@@ -4,12 +4,12 @@
 # Downloads artifacts metadata from the circle CI (in JSON format).
 download_artifacts_json() {
     local target_file="$1"
-    ARTIFACTS_API_URL="https://circleci.com/api/v2/project/github/getoutreach/${CIRCLE_PROJECT_REPONAME}/${CIRCLE_BUILD_NUM}/artifacts"
+    ARTIFACTS_API_URL="https://circleci.com/api/v1.1/project/github/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/${CIRCLE_BUILD_NUM}/artifacts"
     echo "Downloading artifacts from ${ARTIFACTS_API_URL}:"
     ARTIFACTS_JSON=$(curl -H "Circle-Token: $CIRCLECI_TOKEN" "${ARTIFACTS_API_URL}")
     echo "${ARTIFACTS_JSON}" > "${target_file}"
-    # print the artifacts content for troubleshooting
-    cat "${ARTIFACTS_JSON}"
+    # also echo the artifacts content to console/log for troubleshooting
+    echo "${ARTIFACTS_JSON}"
 }
 
 # extracts specific artifact URL from the artifacts metadata (JSON format)
@@ -17,7 +17,7 @@ get_artifact_url() {
     local artifacts_json_file="$1"
     local path="$2"
 
-    ARTIFACT_URL=$(jq -r '.items[] | select(.path=="'"${path}"'") | .url' "${artifacts_json_file}")
+    ARTIFACT_URL=$(jq -r '.[] | select(.path=="'"${path}"'") | .url' "${artifacts_json_file}")
     echo "${ARTIFACT_URL}"
 }
 
