074dc364216b5c3874763715c01f7aac87696538
Nissim Natanov <nissim.natanov@outreach.io>
2021-07-12 15:08:05 -0700

    move webhook URL to circleci secret context

--- shell/lib/opslevel.sh
+++ shell/lib/opslevel.sh
@@ -4,11 +4,11 @@
 # Uploads a custom event to the webhook of the OpsLevel integration.
 # See upload_prismaci.sh for usage sample.
 upload_custom_event_to_opslevel() {
-    local event_id="$1"
+    local webhook_url="$1"
     local event_json_file="$2"
 
     curl \
-        -X POST "https://app.opslevel.com/integrations/custom_event/${event_id}" \
+        -X POST "${webhook_url}" \
         -H 'content-type: application/json' \
         --data-binary "@${event_json_file}"
 }
--- shell/opslevel/upload_prismaci.sh
+++ shell/opslevel/upload_prismaci.sh
@@ -8,7 +8,12 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 IMAGE_SCAN_ARTIFACT_NAME="tmp/test-results/image_scan.json"
 SCAN_RESULTS_FILE="/tmp/image_scan.json"
 SCAN_RESULTS_OPSLEVEL_FILE="/tmp/image_scan_opslevel.json"
-OPSLEVEL_PRISMACI_CUSTOM_EVENT_ID="625cd47f-257a-4ed4-ac56-7efb4dc25158"
+
+
+if [[ -z $OL_PC_WEBHOOK_URL ]]; then
+  echo "To enable Prisma CI scan summary upload to OpsLevel, set OL_PC_WEBHOOK_URL to the integration web hook URL"
+  exit 0
+fi
 
 ARTIFACTS_JSON_FILE="$1"
 ARTIFACT_URL=$(get_artifact_url "${ARTIFACTS_JSON_FILE}" "${IMAGE_SCAN_ARTIFACT_NAME}")
@@ -29,4 +34,10 @@ jq \
     "${SCAN_RESULTS_FILE}" \
 > "${SCAN_RESULTS_OPSLEVEL_FILE}"
 
-upload_custom_event_to_opslevel "${OPSLEVEL_PRISMACI_CUSTOM_EVENT_ID}" "${SCAN_RESULTS_OPSLEVEL_FILE}"
+# log the summary for troubleshooting, it is short
+echo "Image Scan summary to be shared with OpsLevel:"
+cat "${SCAN_RESULTS_OPSLEVEL_FILE}"
+echo "Uploading the summary to ${OL_PC_WEBHOOK_URL}:"
+upload_custom_event_to_opslevel "${OL_PC_WEBHOOK_URL}" "${SCAN_RESULTS_OPSLEVEL_FILE}"
+echo ""
+echo "Prisma CI scan summary has been uploaded to OpsLevel!"
