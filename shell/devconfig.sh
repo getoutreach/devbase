6b889d08661487a0e5ccfed69ddbe5dd259f6b13
Jared Allard <jaredallard@users.noreply.github.com>
2021-07-12 23:15:51 +0000

    use vault-dev, but dont regen credentials

--- shell/devconfig.sh
+++ shell/devconfig.sh
@@ -20,8 +20,6 @@ mkdir -p "$configDir"
 
 VAULT_ADDR=https://vault.outreach.cloud
 if [[ -n $CI ]]; then
-  # shellcheck source=./circleci/setup-vault.sh
-  source "$DIR/circleci/setup-vault.sh"
   VAULT_ADDR=https://vault-dev.outreach.cloud
 fi
 export VAULT_ADDR
