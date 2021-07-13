039a57eddd9b132c026bb0191d797cf1bb2746d5
Nissim Natanov <nissim.natanov@outreach.io>
2021-07-13 08:58:05 -0700

    trying PAT

--- shell/lib/circleci-api.sh
+++ shell/lib/circleci-api.sh
@@ -4,7 +4,7 @@
 #
 
 # Helper method to retrieve Circle CI API token, it tries various common env variables that store Circle CI
-# API tokens: first CIRCLECI_TOKEN, then CIRCLE_TOKEN and the honeycomb's BUILDEVENT_CIRCLE_API_TOKEN.
+# API tokens: first CIRCLECI_TOKEN, then CIRCLE_TOKEN and the CIRCLE_API_TOKEN.
 # If neither is detected this method exits with an error.
 get_circleci_api_token() {
     local token="${CIRCLECI_TOKEN}"
@@ -12,10 +12,10 @@ get_circleci_api_token() {
         echo "CIRCLECI_TOKEN env value not available, trying CIRCLE_TOKEN"
         token="${CIRCLE_TOKEN}"
         if [[ -z ${token} ]]; then
-            echo "CIRCLE_TOKEN env value also not available, trying honeycombio's BUILDEVENT_CIRCLE_API_TOKEN"
-            token="${BUILDEVENT_CIRCLE_API_TOKEN}"
+            echo "CIRCLE_TOKEN env value also not available, trying CIRCLE_API_TOKEN"
+            token="${CIRCLE_API_TOKEN}"
             if [[ -z ${token} ]]; then
-                echo "BUILDEVENT_CIRCLE_API_TOKEN env value also not available, please set either CIRCLECI_TOKEN or CIRCLE_TOKEN"
+                echo "CIRCLE_API_TOKEN env value also not available, please set either of the env variables"
                 exit 1
             fi
         fi
