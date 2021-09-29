458696a70072636b90442335342e8bfe7a890747
Mark Lee <mark.lee@outreach.io>
2021-09-29 15:28:42 -0700

    Run shfmt

--- shell/codeowners-validator.sh
+++ shell/codeowners-validator.sh
@@ -12,17 +12,17 @@ source "$DIR/lib/bootstrap.sh"
 
 CODEOWNERS_CHECKS="syntax,files,duppatterns,owners"
 if [[ -n $OUTREACH_GITHUB_TOKEN ]]; then
-  GH_TOKEN="$OUTREACH_GITHUB_TOKEN"
+	GH_TOKEN="$OUTREACH_GITHUB_TOKEN"
 elif [[ -f "$HOME/.outreach/github.token" ]]; then
-  GH_TOKEN="$(cat "$HOME/.outreach/github.token")"
+	GH_TOKEN="$(cat "$HOME/.outreach/github.token")"
 else
-  warn "No GitHub token found, skipping owner checks"
-  CODEOWNERS_CHECKS="syntax,files,duppatterns"
+	warn "No GitHub token found, skipping owner checks"
+	CODEOWNERS_CHECKS="syntax,files,duppatterns"
 fi
 
 # TODO: use org value instead of hardcoding
 OWNER_CHECKER_REPOSITORY="getoutreach/$(get_app_name)" \
-  REPOSITORY_PATH="$(get_repo_directory)" \
-  CHECKS="$CODEOWNERS_CHECKS" \
-  GITHUB_ACCESS_TOKEN="$GH_TOKEN" \
-  exec "$GOBIN" "github.com/mszostok/codeowners-validator@v$(get_application_version "codeowners-validator")"
+REPOSITORY_PATH="$(get_repo_directory)" \
+CHECKS="$CODEOWNERS_CHECKS" \
+GITHUB_ACCESS_TOKEN="$GH_TOKEN" \
+	exec "$GOBIN" "github.com/mszostok/codeowners-validator@v$(get_application_version "codeowners-validator")"
