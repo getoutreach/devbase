3511074aa1d5f84d59034fe5bef787ecc9e4b7a9
Jared Allard <jared.allard@outreach.io>
2021-11-09 17:50:16 -0800

    again

--- shell/lib/docker-authn.sh
+++ shell/lib/docker-authn.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # DEPRECATED: Use below path instead
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-source "$DIR/../ci/auth/gcr.sh"
+exec "$DIR/../ci/auth/gcr.sh"
--- shell/lib/ssh-auth.sh
+++ shell/lib/ssh-auth.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # DEPRECATED: Use below path instead
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-source "$DIR/../ci/auth/ssh.sh"
+exec "$DIR/../ci/auth/ssh.sh"
