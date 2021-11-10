33f1c7d8ff7ac3d8d3719d8832f0ca3119a3b468
Jared Allard <jared.allard@outreach.io>
2021-11-09 18:25:35 -0800

    move vault to machine deps

--- shell/ci/e2e/setup-e2e-deps.sh
+++ shell/ci/e2e/setup-e2e-deps.sh
@@ -2,12 +2,6 @@
 # Setup dependencies for e2e tests
 set -e
 
-# Vault
-curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
-apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-apt-get update
-apt-get install -y vault
-
 # kubecfg
 curl -fsSL https://github.com/getoutreach/kubecfg/releases/download/v0.17.0/kubecfg-linux-amd64 >"/usr/local/bin/kubecfg"
 chmod +x /usr/local/bin/kubecfg
