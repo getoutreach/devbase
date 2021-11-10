a5c2f8a5ba41ccf3306fe00ad9d0d3d3f60782cb
Jared Allard <jared.allard@outreach.io>
2021-11-09 17:13:25 -0800

    move away from docker crazy e2e runner

--- shell/e2e.sh
+++ shell/e2e.sh
@@ -6,16 +6,6 @@ if [[ $CI == "true" ]]; then
   "$DIR/circleci/setup-e2e-deps.sh"
   "$DIR/circleci/setup-vault.sh"
   "$DIR/circleci/setup-e2e.sh"
-
-  # Bootstrap puts this here. We could def make this better.
-  # sudo is used here because CI has to do some docker perm hacks
-  sudo docker exec --user circleci -w /host_mnt -it \
-    -e CI=true -e "VAULT_ADDR=$VAULT_ADDR" \
-    -e "TEST_TAGS=$TEST_TAGS" -e "MY_NAMESPACE=$MY_NAMESPACE" -e "MY_POD_SERVICE_ACCOUNT=$MY_POD_SERVICE_ACCOUNT" \
-    -e "OUTREACH_DOMAIN=$OUTREACH_DOMAIN" -e "OUTREACH_ACCOUNTS_BASE_URL=$OUTREACH_ACCOUNTS_BASE_URL" \
-    -e KUBECONFIG="/home/circleci/.outreach/kubeconfig.yaml" \
-    devenv bash \
-    -c "eval \"$(ssh-agent)\"; .bootstrap/shell/ci/auth/ssh.sh; ./scripts/shell-wrapper.sh gobin.sh 'github.com/getoutreach/devbase/e2e@$(cat "$DIR/../.version")'"
-else
-  exec "$("$DIR/gobin.sh" -p "github.com/getoutreach/devbase/e2e@$(cat "$DIR/../.version")")"
 fi
+
+exec "$("$DIR/gobin.sh" -p "github.com/getoutreach/devbase/e2e@$(cat "$DIR/../.version")")"
