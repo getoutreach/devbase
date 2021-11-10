edf8a61c6812a38a3fcececbfdcb3ab292158f68
Jared Allard <jared.allard@outreach.io>
2021-11-09 18:35:32 -0800

    prevent messing up perms

--- shell/ci/e2e/setup-e2e.sh
+++ shell/ci/e2e/setup-e2e.sh
@@ -9,12 +9,14 @@ source "$DIR/../../lib/logging.sh"
 
 info "Setting up devenv container"
 # Pull the devenv out of the container
-docker run --entrypoint bash gcr.io/outreach-docker/devenv:v1.22.0 -c 'cat "$(command -v devenv)"' >devenv
-sudo mv devenv /usr/local/bin/devenv
-sudo chmod +x /usr/local/bin/devenv
-sudo chown circleci:circleci /usr/local/bin/devenv
+docker run --rm --entrypoint bash gcr.io/outreach-docker/devenv:v1.22.0 -c 'cat "$(command -v devenv)"' >devenv
 
 # Allow the devenv to update itself
 info "Updating devenv (if needed)"
-sudo -E devenv --force-update-check status || true
-devenv --version
+./devenv --force-update-check status || true
+./devenv --version
+
+info "Moving devenv into PATH"
+sudo mv devenv /usr/local/bin/devenv
+sudo chmod +x /usr/local/bin/devenv
+sudo chown circleci:circleci /usr/local/bin/devenv
