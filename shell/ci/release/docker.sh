5c11cdd9738cf61f5ee8af29e46af7ed56c723b6
Jared Allard <jared.allard@outreach.io>
2021-11-09 16:20:54 -0800

    better debugging for docker.sh

--- shell/ci/release/docker.sh
+++ shell/ci/release/docker.sh
@@ -5,7 +5,7 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 LIB_DIR="${DIR}/../../lib"
 SEC_DIR="${DIR}/../../security"
-TWIST_SCAN_DIR="${SEC_DIR}/../../prismaci"
+TWIST_SCAN_DIR="${SEC_DIR}/prismaci"
 VERSION="$(make version)"
 
 # shellcheck source=../../lib/bootstrap.sh
@@ -35,15 +35,19 @@ args=(
 # Build a quick native image and load it into docker cache for security scanning
 # Scan reports for release images are also uploaded to OpsLevel (test image reports only available on PR runs as artifacts).
 info "Building Docker Image (for scanning)"
-docker buildx build "${args[@]}" "${secrets[@]}" -t "${appName}" --load .
+(
+  set +x
+  docker buildx build "${args[@]}" "${secrets[@]}" -t "${appName}" --load .
+)
 
 info "🔐 Scanning docker image for vulnerabilities"
 "${TWIST_SCAN_DIR}/twist-scan.sh" "${appName}" || echo "Warning: Failed to scan image"
 
 if [[ -n $CIRCLE_TAG ]]; then
   echo "🔨 Building and Pushing Docker Image (production)"
-  set -x
-  docker buildx build "${args[@]}" "${secrets[@]}" --platform linux/arm64,linux/amd64 \
-    -t "${remote_image_name}:${VERSION}" -t "$remote_image_name:latest" --push .
-  set +x
+  (
+    set +x
+    docker buildx build "${args[@]}" "${secrets[@]}" --platform linux/arm64,linux/amd64 \
+      -t "${remote_image_name}:${VERSION}" -t "$remote_image_name:latest" --push .
+  )
 fi
