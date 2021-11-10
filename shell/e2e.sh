35594e852775cc05de3e39a3de84e62cdbb0cd01
Jared Allard <jared.allard@outreach.io>
2021-11-09 17:22:31 -0800

    fix path

--- shell/e2e.sh
+++ shell/e2e.sh
@@ -3,9 +3,8 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 if [[ $CI == "true" ]]; then
-  "$DIR/circleci/setup-e2e-deps.sh"
-  "$DIR/circleci/setup-vault.sh"
-  "$DIR/circleci/setup-e2e.sh"
+  "$DIR/ci/e2e/setup-e2e-deps.sh"
+  "$DIR/ci/e2e/setup-e2e.sh"
 fi
 
 exec "$("$DIR/gobin.sh" -p "github.com/getoutreach/devbase/e2e@$(cat "$DIR/../.version")")"
