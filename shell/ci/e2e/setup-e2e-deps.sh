8470c33a184eb882735842aa6877610dfa5e9a5c
Jared Allard <jared.allard@outreach.io>
2021-11-09 19:02:14 -0800

    install kubectl

--- shell/ci/e2e/setup-e2e-deps.sh
+++ shell/ci/e2e/setup-e2e-deps.sh
@@ -2,6 +2,12 @@
 # Setup dependencies for e2e tests
 set -e
 
+# kubectl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
 # kubecfg
 curl -fsSL https://github.com/getoutreach/kubecfg/releases/download/v0.17.0/kubecfg-linux-amd64 >"/usr/local/bin/kubecfg"
 chmod +x /usr/local/bin/kubecfg
