0e9075f8fbdb36e294f95893f61d98674371aef0
Jared Allard <jared.allard@outreach.io>
2021-11-09 18:39:14 -0800

    chmod +x

--- shell/ci/e2e/setup-e2e.sh
+++ shell/ci/e2e/setup-e2e.sh
@@ -7,16 +7,16 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 # shellcheck source=../../lib/logging.sh
 source "$DIR/../../lib/logging.sh"
 
-info "Setting up devenv container"
+info "Setting up devenv"
 # Pull the devenv out of the container
 docker run --rm --entrypoint bash gcr.io/outreach-docker/devenv:v1.22.0 -c 'cat "$(command -v devenv)"' >devenv
 
 # Allow the devenv to update itself
 info "Updating devenv (if needed)"
+chmod +x ./devenv
 ./devenv --force-update-check status || true
 ./devenv --version
 
 info "Moving devenv into PATH"
 sudo mv devenv /usr/local/bin/devenv
-sudo chmod +x /usr/local/bin/devenv
 sudo chown circleci:circleci /usr/local/bin/devenv
