85b5a56fa054d6b3aec96f8fb3270afbfc67258a
Mark Lee <malept@users.noreply.github.com>
2021-12-13 14:47:38 -0800

    fix(hjson-sync): use correct function name

--- shell/ci/testing/sync-package-hjson.js
+++ shell/ci/testing/sync-package-hjson.js
@@ -66,7 +66,7 @@ if (match === null) {
 
         let githubToken;
         try {
-          githubToken = readFileSync(githubTokenPath);
+          githubToken = fs.readFileSync(githubTokenPath);
         } catch (err) {
           console.error("No GitHub token found:", err);
           process.exit();
