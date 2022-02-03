68f201f0f59c97bcc3242fdc105f8d781a5e4b36
George Shaw <george.e.shaw.iv@outlook.com>
2022-02-02 22:09:00 -0800

    script fixes

--- shell/ci/release/confluence-publish.sh
+++ shell/ci/release/confluence-publish.sh
@@ -12,6 +12,7 @@ set -e
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 LIB_DIR="${DIR}/../../lib"
+GOBIN="${DIR}/../../gobin.sh"
 
 # shellcheck source=../../lib/logging.sh
 source "${LIB_DIR}/logging.sh"
@@ -22,8 +23,8 @@ source "${LIB_DIR}/bootstrap.sh"
 # shellcheck source=../../lib/shell.sh
 source "${LIB_DIR}/shell.sh"
 
-srcPath=$DIR
-info "srcPath: ${DIR}"
+srcPath=$(get_repo_directory)
+info "srcPath: ${srcPath}"
 
 tag=$(eval "git describe --tags --abbrev=0")
 findCmd="git diff-tree --no-commit-id --name-status -r ${tag} HEAD | grep -e '^[A|M].*\.md$' | cut -f2"
@@ -36,10 +37,9 @@ info "defaultBranch: ${defaultBranch}"
 
 for file in $(eval "${findCmd}"); do
   info "inspecting found markdown file: ${file}"
-  if grep -q '^\s*<!--\s*Space:\s*.+\s*-->\s*$' "${file}"; then
-    info "found space directive in ${file}:\n---"
-    grep '^\s*<!--\s*Space:\s*.+\s*-->\s*$' "${file}" # Re-output this w/o quiet flag for debugging purposes.
-    info "---"
+  if grep -Eq '^\s*<!--\s*Space:\s*.+\s*-->\s*$' "${file}"; then
+    info_sub "found space directive in ${file}:"
+    info_sub "$(grep -E '^\s*<!--\s*Space:\s*.+\s*-->\s*$' "${file}")" # Re-output this w/o quiet flag for debugging purposes.
 
     {
       echo ""
@@ -47,26 +47,33 @@ for file in $(eval "${findCmd}"); do
       echo "Do not edit! File is auto-generated from [github](https://github.com/getoutreach/$(get_app_name)/blob/${defaultBranch}/${file})."
     } >>"$file"
 
-    fullname="${srcPath}/${file}"
-    filedirname=$(dirname "$fullname")
-    filebasename=$(basename "$fullname")
-    updatedname="${filebasename}.updated.md"
+    fullName="${srcPath}/${file}"
+    fileDirName=$(dirname "$fullName")
+    fileBaseName=$(basename "$fullName")
+    updatedName="${fileBaseName}.updated.md"
 
-    info_sub "dir: ${filedirname}"
-    info_sub "basename: ${filebasename}"
-    info_sub "updatedname: ${updatedname}"
+    info_sub "dir: ${fileDirName}"
+    info_sub "baseName: ${fileBaseName}"
+    info_sub "updatedName: ${updatedName}"
 
     # Enter the directory of the md file to run command since the generated plots are relative path
-    pushd "$filedirname" || exit
+    pushd "$fileDirName" || exit
 
     # Render
-    "${srcPath}/scripts/shell-wrapper.sh gobin.sh github.com/getoutreach/markdowntools/cmd/visualizemd@$(get_application_version "getoutreach/markdowntools/visualizemd") -umlusername internal_access_user -umlpassword ${UML_PASSWORD} -umlserver https://rolling.mi.outreach-dev.com/api/internal/plantuml -u ${CONFLUENCE_USERNAME} -p ${CONFLUENCE_API_TOKEN} -f ${filebasename} > ${updatedname}"
-    cat "$updatedname"
+    "$GOBIN" "github.com/getoutreach/markdowntools/cmd/visualizemd@$(get_application_version "getoutreach/markdowntools/visualizemd")" \
+      -umlusername internal_access_user -umlpassword "${UML_PASSWORD}" -umlserver https://rolling.mi.outreach-dev.com/api/internal/plantuml \
+      -u "${CONFLUENCE_USERNAME}" -p "${CONFLUENCE_API_TOKEN}" -f "${fileBaseName}" >"${updatedName}"
+
+    cat "$updatedName"
 
     # Push to confluence
-    retry 5 5 "${srcPath}/scripts/shell-wrapper.sh gobin.sh github.com/kovetskiy/mark@$(get_application_version "kovetskiy/mark") --minor-edit -u ${CONFLUENCE_USERNAME} -p ${CONFLUENCE_API_TOKEN} -b https://outreach-io.atlassian.net/wiki -f ${updatedname} --debug"
+    retry 5 5 "$GOBIN" "github.com/kovetskiy/mark@$(get_application_version "kovetskiy/mark")" \
+      --minor-edit -k -u "${CONFLUENCE_USERNAME}" -p "${CONFLUENCE_API_TOKEN}" -b https://outreach-io.atlassian.net/wiki \
+      -f "${updatedName}"
 
     # Return to source directory.
     popd
+  else
+    info_sub "no space directive found, skipping ${file}"
   fi
 done
