3f57dfac2f6d36b1993fb4e9c29be09a73d5094f
Peter Lisy <peter.lisy@outreach.io>
2022-03-11 17:49:45 -0800

    feat: Added support for app image registry override

--- shell/build-jsonnet.sh
+++ shell/build-jsonnet.sh
@@ -5,29 +5,30 @@ SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 # shellcheck source=./lib/bootstrap.sh
 source "$SCRIPTS_DIR/lib/bootstrap.sh"
 
-APPNAME="$(get_app_name)"
-
 # shellcheck source=./lib/logging.sh
 source "$SCRIPTS_DIR/lib/logging.sh"
 
 action=$1
 
+appName="${DEVENV_DEPLOY_APPNAME:-$(get_app_name)}"
 bento="${DEVENV_DEPLOY_BENTO:-"bento1a"}"
-namespace="${DEVENV_DEPLOY_NAMESPACE:-$APPNAME--$bento}"
+namespace="${DEVENV_DEPLOY_NAMESPACE:-$appName--$bento}"
 version="${DEVENV_DEPLOY_VERSION:-"latest"}"
 environment="${DEVENV_DEPLOY_ENVIRONMENT:-"development"}"
 host="${DEVENV_DEPLOY_HOST:-"bento1a.outreach-dev.com"}"
 email="${DEVENV_DEPLOY_DEV_EMAIL:-$(git config user.email)}"
+appImageRegistry="${DEVENV_DEPLOY_IMAGE_REGISTRY:-"gcr.io/outreach-docker"}"
 
 kubecfg \
   --jurl http://k8s-clusters.outreach.cloud/ \
   --jurl https://raw.githubusercontent.com/getoutreach/jsonnet-libs/master \
   -n "$namespace" \
-  --context "dev-environment" "$action" "$(get_repo_directory)/deployments/$APPNAME/$APPNAME.jsonnet" \
+  --context "dev-environment" "$action" "$(get_repo_directory)/deployments/$appName/$appName.jsonnet" \
   -V cluster="development.us-west-2" \
   -V namespace="$namespace" \
   -V environment="$environment" \
   -V version="$version" \
   -V bento="$bento" \
   -V dev_email="$email" \
-  -V host="$host"
+  -V host="$host" \
+  -A appImageRegistry="$appImageRegistry" 
