f413833a21aba57ce124b51c8a324f53cb58e2ca
Peter Lisy <peter.lisy@outreach.io>
2022-03-24 21:48:50 -0700

    pr: make linter gods happy

--- shell/debug.sh
+++ shell/debug.sh
@@ -12,7 +12,7 @@ export CGO_ENABLED=1
 
 set -ex
 
-if [[ -n "${DLV_PORT}" ]]; then
+if [[ -n ${DLV_PORT} ]]; then
   exec "$SCRIPTS_DIR/gobin.sh" github.com/go-delve/delve/cmd/dlv@v"$(get_application_version "delve")" debug "$(get_repo_directory)/cmd/$(get_app_name)" --headless --listen=":${DLV_PORT}"
 else
   exec "$SCRIPTS_DIR/gobin.sh" github.com/go-delve/delve/cmd/dlv@v"$(get_application_version "delve")" debug --build-flags="-tags=or_dev" "$(get_repo_directory)/cmd/$(get_app_name)"
