519adccac6b2ce0e52f3fa275e08d8fbf651d2a7
Peter Lisy <peter.lisy@outreach.io>
2022-03-25 12:24:01 -0700

    made things better

--- shell/debug.sh
+++ shell/debug.sh
@@ -13,7 +13,7 @@ export CGO_ENABLED=1
 set -ex
 
 if [[ -n ${DLV_PORT} ]]; then
-  exec "$SCRIPTS_DIR/gobin.sh" github.com/go-delve/delve/cmd/dlv@v"$(get_application_version "delve")" debug "$(get_repo_directory)/cmd/$(get_app_name)" --headless --listen=":${DLV_PORT}"
+  exec "$SCRIPTS_DIR/gobin.sh" github.com/go-delve/delve/cmd/dlv@v"$(get_application_version "delve")" exec "$(get_repo_directory)/bin/$(get_app_name)" --headless --listen=":${DLV_PORT}"
 else
   exec "$SCRIPTS_DIR/gobin.sh" github.com/go-delve/delve/cmd/dlv@v"$(get_application_version "delve")" debug --build-flags="-tags=or_dev" "$(get_repo_directory)/cmd/$(get_app_name)"
 fi
