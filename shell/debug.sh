6532ab6f17a05e9cf9b3a550526bf4e33740b161
Peter Lisy <peter.lisy@outreach.io>
2022-03-24 21:37:12 -0700

    feat(devspace): Enable remote debugging using vscode

--- shell/debug.sh
+++ shell/debug.sh
@@ -12,4 +12,8 @@ export CGO_ENABLED=1
 
 set -ex
 
-exec "$SCRIPTS_DIR/gobin.sh" github.com/go-delve/delve/cmd/dlv@v"$(get_application_version "delve")" debug --build-flags="-tags=or_dev" "$(get_repo_directory)/cmd/$(get_app_name)"
+if [[ -n "${DLV_PORT}" ]]; then
+  exec "$SCRIPTS_DIR/gobin.sh" github.com/go-delve/delve/cmd/dlv@v"$(get_application_version "delve")" debug "$(get_repo_directory)/cmd/$(get_app_name)" --headless --listen=":${DLV_PORT}"
+else
+  exec "$SCRIPTS_DIR/gobin.sh" github.com/go-delve/delve/cmd/dlv@v"$(get_application_version "delve")" debug --build-flags="-tags=or_dev" "$(get_repo_directory)/cmd/$(get_app_name)"
+fi
