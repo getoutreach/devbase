2833bc1239df2938687390fb6a1e953208e9e96e
Richard Larocque <richard.larocque@outreach.io>
2022-05-20 09:44:30 -0700

    feat: Add 'asdf-exec.sh' and 'go.sh'
    Adds an `asdf-exec.sh` script that will `asdf exec` the given arguments
    in an environment that respects the embedding project's `.tool-versions`
    file.
    
    In a perfect world, this functionality would be baked in to `asdf`
    upstream.  For now we build this functionality from hacky shell scripts.
    It may break in some edge case scenarios, but it should be good enough
    for all the situations we're likely to encounter int he real world.
    
    Also adds a `go.sh` that does `asdf-exec.sh go`.  This is intended to
    provide compatibilty for tools require us to point at the "Go
    executable".

---
+++ shell/asdf-exec.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Runs the provided tool in an `asdf exec` under an environment that respects
+# the tool versions defined in this project's `.tool-versions` file.
+#
+# This allows scripts to invoke the "right" version of a tool for the given
+# project without having to source `asdf`'s setup scripts or be in any
+# particular directory.
+#
+# The idea here is that `some_project/.bootstrap/shell/exec.sh go` will always
+# invoke the right go version for that particular `some_project`.
+
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+# shellcheck source=./lib/bootstrap.sh
+source "$SCRIPTS_DIR/lib/bootstrap.sh"
+
+TOOLVERSIONS="$(get_repo_directory)/.tool-versions"
+
+while read -r line; do
+  tool=$(echo "$line" | awk '{ print $1 }' | tr '[:lower:]-' '[:upper:]_')
+  version=$(echo "$line" | awk '{ print $2 }')
+
+  export "ASDF_${tool}_VERSION"="${version}"
+done < <(grep -v '^#' "${TOOLVERSIONS}")
+
+asdf exec "$@"
+
