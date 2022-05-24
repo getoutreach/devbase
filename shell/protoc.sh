67102ac75dd9efa3baaa61a6f1a856895b2243a4
George Shaw <george.e.shaw.iv@outlook.com>
2022-05-24 15:15:14 -0700

    quote things

--- shell/protoc.sh
+++ shell/protoc.sh
@@ -16,7 +16,7 @@ mkdir -p "$PROTOC_IMPORTS_BASE_PATH"
 
 info "Ensuring protoc imports are avaliable locally"
 
-imports=$(get_list "go-protoc-imports")
+imports="$(get_list "go-protoc-imports")"
 
 # Add default imports so they can get dealt with in the following loop just like custom imports.
 if [[ has_feature "validation" ]]; then
@@ -24,7 +24,7 @@ if [[ has_feature "validation" ]]; then
 fi
 
 protoc_imports=()
-for import in $(get_list "go-protoc-imports"); do
+for import in "$imports"; do
   module_version_str=$(jq -r .module <<<"$import")
   info_sub "$module_version_str"
 
@@ -61,28 +61,28 @@ done
 info "Ensuring protoc plugins are installed"
 
 info_sub "protoc-gen-validate"
-protoc_gen_validate=$($GOBIN -p github.com/envoyproxy/protoc-gen-validate@v0.6.7)
+protoc_gen_validate="$($GOBIN -p github.com/envoyproxy/protoc-gen-validate@v0.6.7)"
 
 info_sub "protoc-gen-go"
-protoc_gen_go=$($GOBIN -p google.golang.org/protobuf/cmd/protoc-gen-go@v1.28)
+protoc_gen_go="$($GOBIN -p google.golang.org/protobuf/cmd/protoc-gen-go@v1.28)"
 
 info_sub "protoc-gen-go-grpc"
-protoc_gen_go_grpc=$($GOBIN -p google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2)
+protoc_gen_go_grpc="$($GOBIN -p google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2)"
 
 info_sub "protoc-gen-doc"
-protoc_gen_doc=$($GOBIN -p github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@v1.5.1)
+protoc_gen_doc="$($GOBIN -p github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@v1.5.1)"
 
 info "Generating GRPC Clients"
 
 # Create the language specific clients
 info_sub "go"
 exec protoc \
-  --plugin=protoc-gen-go=$protoc_gen_go --plugin=protoc-gen-go-grpc=$protoc_gen_go_grpc \
+  --plugin=protoc-gen-go="$protoc_gen_go" --plugin=protoc-gen-go-grpc="$protoc_gen_go_grpc" \
   --go_out=. --go_opt=paths=source_relative \
   --go-grpc_out=. --go-grpc_opt=paths=source_relative \
   $(for i in "${protoc_imports[@]}"; do echo "--proto_path=$i"; done) \
   $(if has_feature "validation"; then echo "--plugin=protoc-gen-validate=$protoc_gen_validate --validate_out=lang=go:$(get_repo_directory)/api"; fi) \
-  --plugin=protoc-gen-doc=$protoc_gen_doc --doc_out="$(get_repo_directory)/api/doc" --doc_opt=html,index.html \
+  --plugin=protoc-gen-doc="$protoc_gen_doc" --doc_out="$(get_repo_directory)/api/doc" --doc_opt=html,index.html \
   --proto_path "$(get_repo_directory)/api" "$(get_repo_directory)/api/"*.proto
 
 mkdir -p "$PROTO_DOCS_DIR"
