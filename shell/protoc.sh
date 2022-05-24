c031923e1d52ce1bf8e1d5464a1e3fa5fabc5df9
George Shaw <george.e.shaw.iv@outlook.com>
2022-05-24 15:07:53 -0700

    deal with protoc imports

--- shell/protoc.sh
+++ shell/protoc.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
-# This script is a light wrapper around the 'docker-protoc' image (see: https://github.com/namely/docker-protoc),
-# which is capable of compiling proto definitions into various languages.
-# This is not meant to be used in CircleCI (except by bootstrap itself to validate the general process).
-IMAGE="gcr.io/outreach-docker/protoc:1.37_2"
-uid=$(id -u)
-gid=$(id -g)
 
 SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+GOBIN="$SCRIPTS_DIR/gobin.sh"
 
 # shellcheck source=./lib/logging.sh
 source "$SCRIPTS_DIR/lib/logging.sh"
@@ -16,79 +11,85 @@ source "$SCRIPTS_DIR/lib/bootstrap.sh"
 
 PROTO_DOCS_DIR="$(get_repo_directory)/apidocs/proto"
 
-# Fallback if uid/gid is somehow empty
-if [[ -z $uid ]] || [[ -z $gid ]]; then
-  echo "Error: Failed to determine the uid/gid of the current user. Defaulting to standard 1000." >&2
-  uid=1000
-  gid=1000
+PROTOC_IMPORTS_BASE_PATH="$HOME/.outreach/.protoc-imports"
+mkdir -p "$PROTOC_IMPORTS_BASE_PATH"
+
+info "Ensuring protoc imports are avaliable locally"
+
+imports=$(get_list "go-protoc-imports")
+
+# Add default imports so they can get dealt with in the following loop just like custom imports.
+if [[ has_feature "validation" ]]; then
+  imports="${imports}{\"module\":\"github.com/envoyproxy/protoc-gen-validate@v0.6.7\",\"path\":\"\"}$'\n'"
 fi
 
-get_import_basename() {
-  # shellcheck disable=SC2155 # Why: splitting declartion is messier
-  local module="$(jq -r .module <<<"$1")"
-  # sed removes the version off the module path if present
-  basename "$(go list -f '{{ .Path }}' -m "$module" | sed 's|/v[0-9]||')"
-}
-get_import_path() {
-  # shellcheck disable=SC2155 # Why: splitting declartion is messier
-  local module="$(jq -r .module <<<"$1")"
-  # sed removes the version off the module path if present
-  module_root="$(go list -f '{{ .Dir }}' -m "$module" | sed 's|/v[0-9]||')"
-  echo "${module_root}$(jq -r .path <<<"$1")"
-}
+protoc_imports=()
 for import in $(get_list "go-protoc-imports"); do
-  module=$(jq -r .module <<<"$import")
-  currentver="$(go version | awk '{ print $3 }' | sed 's|go||')"
-  requiredver="1.16.0"
-  if [ ! "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then
-    echo "Go version must be greater than ${requiredver} to use 'go-protoc-imports' feature"
-    exit 1
-  fi
-  go install "$module"
-  # check exit for this instead of the install itself, as the install can
-  # return non-zero, but still retrieve the dependency. As long as we can
-  # retrieve the dep with go list, we can import the files for protoc
-  go list -f '{{ .Dir }}' -m "$module"
-  exit_code=$?
-  if [[ $exit_code != "0" ]]; then
-    exit $exit_code
-  fi
-done
+  module_version_str=$(jq -r .module <<<"$import")
+  info_sub "$module_version_str"
 
-# This is where the plugins will be installed, the path could definitely change
-# since we're using asdf to manage go versions.
-goenvbin="$(go env GOPATH)/bin"
+  module_version_arr=(${module_version_str//@/ })
+  module=${module_version_arr[0]}
+  version="latest"
+
+  if [[ ${#module_version_arr[@]} -eq 2 ]]; then
+    version=${module_version_arr[1]}
+  fi
+
+  if [[ $version == "latest" ]]; then
+    version="$(gh release -R $module list -L 1 | awk '{print $1}')"
+  fi
+
+  import_path="$PROTOC_IMPORTS_BASE_PATH/$module/$version"
+  path=$(jq -r .path <<<"$import")
+  path=${path#"/"} # Trim / prefix if it exists.
+
+  # Add import to list
+  protoc_imports+=("$import_path/$path")
+  
+  # Check to see if we already have this version locally and skip cloning
+  # if we already do.
+  if [[ -d "$import_path" ]]; then
+    continue
+  fi
+
+  # Clone import into the import path since we don't have it already
+  mkdir -p "$import_path"
+  git clone --depth 1 --branch "$version" "$module" "$import_path"
+done
 
 info "Ensuring protoc plugins are installed"
 
 info_sub "protoc-gen-validate"
-go get -d github.com/envoyproxy/protoc-gen-validate@v0.6.7
-go install github.com/envoyproxy/protoc-gen-validate@v0.6.7
+protoc_gen_validate=$($GOBIN -p github.com/envoyproxy/protoc-gen-validate@v0.6.7)
 
 info_sub "protoc-gen-go"
-go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
+protoc_gen_go=$($GOBIN -p google.golang.org/protobuf/cmd/protoc-gen-go@v1.28)
 
 info_sub "protoc-gen-go-grpc"
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+protoc_gen_go_grpc=$($GOBIN -p google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2)
 
 info_sub "protoc-gen-doc"
-go install github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@v1.5.1
+protoc_gen_doc=$($GOBIN -p github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@v1.5.1)
 
 info "Generating GRPC Clients"
 
 # Create the language specific clients
 info_sub "go"
 exec protoc \
-  --plugin=protoc-gen-go=$goenvbin/protoc-gen-go --plugin=protoc-gen-go-grpc=$goenvbin/protoc-gen-go-grpc \
+  --plugin=protoc-gen-go=$protoc_gen_go --plugin=protoc-gen-go-grpc=$protoc_gen_go_grpc \
   --go_out=. --go_opt=paths=source_relative \
   --go-grpc_out=. --go-grpc_opt=paths=source_relative \
-  $(for import in $(get_list "go-protoc-imports"); do echo "--proto_path=/mod/$(get_import_basename "$import")"; done) \
-  $(if has_feature "validation"; then echo "--proto_path=$(go env GOPATH)/src/github.com/envoyproxy/protoc-gen-validate --plugin=protoc-gen-validate=$goenvbin/protoc-gen-validate --validate_out=lang=go:$(get_repo_directory)/api"; fi) \
-  --plugin=protoc-gen-doc=$goenvbin/protoc-gen-doc --doc_out="$(get_repo_directory)/api/doc" --doc_opt=html,index.html \
+  $(for i in "${protoc_imports[@]}"; do echo "--proto_path=$i"; done) \
+  $(if has_feature "validation"; then echo "--plugin=protoc-gen-validate=$protoc_gen_validate --validate_out=lang=go:$(get_repo_directory)/api"; fi) \
+  --plugin=protoc-gen-doc=$protoc_gen_doc --doc_out="$(get_repo_directory)/api/doc" --doc_opt=html,index.html \
   --proto_path "$(get_repo_directory)/api" "$(get_repo_directory)/api/"*.proto
 
 mkdir -p "$PROTO_DOCS_DIR"
 mv "$(get_repo_directory)"/api/doc/index.html "$PROTO_DOCS_DIR"
+
+# Legacy below this, need to deal with.
+IMAGE="gcr.io/outreach-docker/protoc:1.37_2"
 
 if has_grpc_client "node"; then
   info_sub "node"
