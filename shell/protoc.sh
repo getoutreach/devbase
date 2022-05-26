2f3036bf1ae96a2a4e4a153a0a4127ba50eb8b25
George Shaw <george.e.shaw.iv@outlook.com>
2022-05-25 17:38:34 -0700

    done i think

--- shell/protoc.sh
+++ shell/protoc.sh
@@ -23,7 +23,7 @@ if has_feature "validation"; then
   imports="${imports}{\"module\":\"github.com/envoyproxy/protoc-gen-validate@v$(get_application_version "protoc-gen-validate")\",\"path\":\"\"}$'\n'"
 fi
 
-protoc_imports=()
+default_args=()
 for import in $imports; do
   module_version_str=$(jq -r .module <<<"$import")
   info_sub "$module_version_str"
@@ -46,7 +46,7 @@ for import in $imports; do
   path=${path#"/"} # Trim / prefix if it exists.
 
   # Add import to list
-  protoc_imports+=("$import_path/$path")
+  default_args+=("--proto-path=$import_path/$path")
 
   # Check to see if we already have this version locally and skip cloning
   # if we already do.
@@ -68,14 +68,29 @@ protoc_gen_go_grpc=$("$GOBIN" -p github.com/grpc/grpc-go/cmd/protoc-gen-go-grpc@
 protoc_gen_doc=$("$GOBIN" -p github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@v"$(get_application_version "protoc-gen-doc")")
 
 info_sub "Running Go protobuf generation"
-protoc \
-  --plugin=protoc-gen-go="$protoc_gen_go" --plugin=protoc-gen-go-grpc="$protoc_gen_go_grpc" \
-  --go_out=. --go_opt=paths=source_relative \
-  --go-grpc_out=. --go-grpc_opt=paths=source_relative \
-  "$(for i in "${protoc_imports[@]}"; do echo "--proto_path=$i"; done)" \
-  "$(if has_feature "validation"; then echo "--plugin=protoc-gen-validate=$protoc_gen_validate --validate_out=lang=go:$(get_repo_directory)/api"; fi)" \
-  --plugin=protoc-gen-doc="$protoc_gen_doc" --doc_out="$(get_repo_directory)/api/doc" --doc_opt=html,index.html \
-  --proto_path "$(get_repo_directory)/api" "$(get_repo_directory)/api/"*.proto
+
+go_args=("${default_args[@]}")
+go_args+=(
+  --plugin=protoc-gen-go="$protoc_gen_go"
+  --plugin=protoc-gen-go-grpc="$protoc_gen_go_grpc"
+  --go_out=.
+  --go_opt=paths=source_relative
+  --go-grpc_out=.
+  --go-grpc_opt=paths=source_relative
+  --plugin=protoc-gen-doc="$protoc_gen_doc"
+  --doc_out="$(get_repo_directory)/api/doc"
+  --doc_opt=html,index.html
+  --proto_path="$(get_repo_directory)/api"
+)
+
+if has_feature "validation"; then
+  go_args+=(
+    --plugin=protoc-gen-validate="$protoc_gen_validate"
+    --validate_out=lang=go:"$(get_repo_directory)/api"
+  )
+fi
+
+protoc "${go_args[@]}" "$(get_repo_directory)/api/"*.proto
 
 mkdir -p "$PROTO_DOCS_DIR"
 mv "$(get_repo_directory)/api/doc/index.html" "$PROTO_DOCS_DIR"
@@ -98,12 +113,16 @@ if has_grpc_client "node"; then
   grpc_tools_node_plugin="$NODE_GRPC_TOOLS_CACHE_DIR/bin/grpc_tools_node_protoc_plugin"
 
   info_sub "Running Node protobuf generation"
-  "$grpc_tools_node_bin" \
-    --plugin=grpc_tools_ruby_protoc_plugin="$grpc_tools_node_plugin" \
-    --js_out=import_style=commonjs,binary:"$(get_repo_directory)/api/clients/node/src/grpc" \
-    --grpc_out=grpc_js:"$(get_repo_directory)/api/clients/node/src/grpc" \
-    "$(for i in "${protoc_imports[@]}"; do echo "--proto_path=$i"; done)" \
-    --proto_path "$(get_repo_directory)/api" "$(get_repo_directory)/api/"*.proto
+
+  node_args=("${default_args[@]}")
+  node_args+=(
+    --plugin=grpc_tools_ruby_protoc_plugin="$grpc_tools_node_plugin"
+    --js_out=import_style=commonjs,binary:"$(get_repo_directory)/api/clients/node/src/grpc"
+    --grpc_out=grpc_js:"$(get_repo_directory)/api/clients/node/src/grpc"
+    --proto_path "$(get_repo_directory)/api"
+  )
+
+  "$grpc_tools_node_bin" "${node_args[@]}" "$(get_repo_directory)/api/"*.proto
 fi
 
 if has_grpc_client "ruby"; then
@@ -117,10 +136,14 @@ if has_grpc_client "ruby"; then
   grpc_tools_ruby_plugin="$(gem env | grep "\- INSTALLATION DIRECTORY" | awk '{print $4}')/gems/grpc-tools-$(get_application_version "ruby-grpc-tools")/bin/grpc_tools_ruby_protoc_plugin"
 
   info_sub "Running Ruby protobuf generation"
-  "$grpc_tools_ruby_bin" \
-    --plugin=grpc_tools_ruby_protoc_plugin="$grpc_tools_ruby_plugin" \
-    --ruby_out="$(get_repo_directory)/api/clients/ruby/lib/$(get_app_name)_client" \
-    --grpc_out="$(get_repo_directory)/api/clients/ruby/lib/$(get_app_name)_client" \
-    "$(for i in "${protoc_imports[@]}"; do echo "--proto_path=$i"; done)" \
-    --proto_path "$(get_repo_directory)/api" "$(get_repo_directory)/api/"*.proto
+
+  ruby_args=("${default_args[@]}")
+  ruby_args+=(
+    --plugin=grpc_tools_ruby_protoc_plugin="$grpc_tools_ruby_plugin"
+    --ruby_out="$(get_repo_directory)/api/clients/ruby/lib/$(get_app_name)_client"
+    --grpc_out="$(get_repo_directory)/api/clients/ruby/lib/$(get_app_name)_client"
+    --proto_path "$(get_repo_directory)/api"
+  )
+
+  "$grpc_tools_ruby_bin" "${ruby_args[@]}" "$(get_repo_directory)/api/"*.proto
 fi
