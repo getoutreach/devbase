21aa8945b578330856cac153085e2314f44a0bc9
George Shaw <george.e.shaw.iv@outlook.com>
2022-06-01 22:48:16 -0700

    feat: gcloud auth

--- shell/ci/auth/gcr.sh
+++ shell/ci/auth/gcr.sh
@@ -11,3 +11,12 @@ docker login \
   -u _json_key \
   --password-stdin \
   https://gcr.io <<<"${GCLOUD_SERVICE_ACCOUNT}"
+
+# Key needs to be written to a file to authorize gcloud CLI.
+echo "$GCLOUD_SERVICE_ACCOUNT" >gcloud-auth-key.json
+
+gcloud auth activate-service-account \
+  circleci-rw@outreach-docker.iam.gserviceaccount.com \
+  --key-file=gcloud-auth-key.json
+
+rm -f gcloud-auth-key.json
