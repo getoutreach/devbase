d398881c8178d561593aa2d863a35abdd973b084
Peter Lisy <peter.lisy@outreach.io>
2022-06-01 18:57:36 -0700

    feat: Surface using logfmt for dev-time logs

--- shell/devspace_start.sh
+++ shell/devspace_start.sh
@@ -48,7 +48,7 @@ COLOR_RESET="\033[0m"
 APPNAME="$(get_app_name)"
 
 BANNER="${COLOR_CYAN}
-  ____              ____
+   ____              ____
   |  _ \  _____   __/ ___| _ __   __ _  ___ ___
   | | | |/ _ \ \ / /\___ \| '_ \ / _\` |/ __/ _ \\
   | |_| |  __/\ V /  ___) | |_) | (_| | (_|  __/
@@ -59,15 +59,16 @@ Welcome to your development container!
 
 This is how you can work with it:
 - Run \`${COLOR_CYAN}make${COLOR_RESET}\` to build the application
+- Run \`${COLOR_CYAN}make run${COLOR_RESET}\` to build and start the server
 - Run \`${COLOR_CYAN}make dev${COLOR_RESET}\` to start the development server with reloading
-- Run \`${COLOR_CYAN}./bin/${APPNAME}${COLOR_RESET}\` to start the server
+- Run \`${COLOR_CYAN}make debug${COLOR_RESET}\` to start the development server with delve attached
+- Try \`${COLOR_CYAN}make [run|dev|debug] LOGFMT=1${COLOR_RESET}\` for more dev friendly terminal logs
 - ${COLOR_CYAN}Files will be synchronized${COLOR_RESET} between your local machine and this container
-- Some ports will be forwarded.
 "
 
 if [[ -z $DEV_CONTAINER_LOGFILE ]] || [[ $DEVENV_DEV_TERMINAL == "true" ]]; then
   echo -e "$BANNER"
   bash
 else
-  make dev | tee -ai "${DEV_CONTAINER_LOGFILE:-/tmp/app.log}"
+  make dev
 fi
