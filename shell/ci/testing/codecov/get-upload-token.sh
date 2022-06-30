8c1f8eb0bba183d21364afef5e0eabc16b412d79
George Shaw <george.e.shaw.iv@outlook.com>
2022-06-29 17:00:51 -0700

    get upload token from codecov api

---
+++ shell/ci/testing/codecov/get-upload-token.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+if [[ -z $CODECOV_API_KEY ]]; then
+  echo "CODECOV_API_KEY environment variable is empty." >&2
+  exit 1
+fi
+
+if [[ $# -ne 2 ]]; then
+  echo "Script expects the owner/organization and name of repository passed to it as parameters." >&2
+  exit 1
+fi
+
+owner="$1"
+repo="$2"
+
+info="$(curl -X GET "https://codecov.io/api/gh/$owner/$repo" -H "Authorization: $CODECOV_API_KEY")"
+
+if [[ $? -ne 1 ]]; then
+  echo "Non-zero exit code returned from curl to codecov to get repository information: $info" >&2
+  exit 1
+fi
+
+# Check to see if the repository is private and not already active. In this case we'll
+# need to manually activate the repository with another call to the codecov API.
+# The truth table for this is the following, with P referring to .repo.prviate and A
+# referring to .repo.active:
+#   P | A || P and (not A)
+#   ----------------------
+#   0 | 0 ||     0
+#   1 | 0 ||     1
+#   0 | 1 ||     0
+#   1 | 1 ||     0
+if [[ $(jq '.repo.private and (.repo.active | not)' <"$info") == "true" ]]; then
+  echo "Attempting to activate private repository."
+  curl -X POST "https://codecov.io/api/pub/gh/$owner/$repo/settings" -d 'action=activate' -H "Authorization: $CODECOV_API_KEY"
+
+  # Re-do the request to get information, upload token should exist now.
+  info="$(curl -X GET "https://codecov.io/api/gh/$owner/$repo" -H "Authorization: $CODECOV_API_KEY")"
+
+  if [[ $? -ne 1 ]]; then
+    echo "Non-zero exit code returned from curl to codecov to get repository information: $info" >&2
+    exit 1
+  fi
+fi
+
+upload_token="$(jq -r '.repo.upload_token' <"$info")"
+if [[ $upload_token == "null" ]]; then
+  upload_token=""
+fi
+
+echo "$upload_token"
---
+++ shell/ci/testing/codecov/upload-coverage.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+if [[ $CIRCLECI == "true" ]]; then
+  owner="${CIRCLE_PROJECT_USERNAME}"
+  repo="${CIRCLE_PR_REPONAME:-$CIRCLE_PROJECT_REPONAME}"
+else
+  if [[ $# -lt 2 ]]; then
+    echo "When running outside of CIRCLECI the first two positional arguments passed to this script need to be owner and repository." >&2
+    exit 1
+  fi
+
+  owner="$1"
+  shift
+  repo="$1"
+  shift
+fi
+
+upload_token="$($DIR/get-upload-token.sh $owner $repo)"
+
+args=("-t" "$upload_token")
+if [[ $# -gt 0 ]]; then
+  args=("${args[@]}" "$@")
+fi
+
+# Install codecov binary if it doesn't already exist in $PATH
+if [[ ! -x "$(command -v codecov)" ]]; then
+  os="linux"
+  if [[ "$(uname)" == "Darwin" ]]; then
+    os="macos"
+  fi
+
+  pushd /usr/local/bin >/dev/null 2>&1 || exit 1
+  curl -Os "https://uploader.codecov.io/latest/$os/codecov"
+  chmod +x codecov
+  popd >/dev/null 2>&1 || exit 1
+fi
+
+if ! codecov "${args[@]}"; then
+  echo "codecov upload failed." >&2
+  exit 1
+fi
+
+echo "Succesfully uploaded codecov report."
+exit 0
