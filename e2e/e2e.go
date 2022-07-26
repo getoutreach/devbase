6f429a2e94e6b4d3146a4731ccabb3f9cce5b919
George Shaw <george.e.shaw.iv@outlook.com>
2022-07-26 15:22:06 -0700

    debugging

--- e2e/e2e.go
+++ e2e/e2e.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"go/build"
 	"os"
 	"os/exec"
@@ -315,6 +316,8 @@ func shouldRunE2ETests() (bool, error) {
 		}
 
 		if info.IsDir() && path != "." {
+			fmt.Println("is dir", path)
+
 			// Skip submodule directories.
 			if hasGitFolder, err := os.Stat(filepath.Join(path, ".git")); err == nil {
 				if hasGitFolder.IsDir() {
