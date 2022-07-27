2da6acc0207a828825205f3a39e0c6da8067d32f
Jared Allard <jared.allard@outreach.io>
2022-07-27 09:52:24 -0700

    support env var for box repo url

--- shell/lib/box.sh
+++ shell/lib/box.sh
@@ -5,7 +5,17 @@ BOXPATH="$HOME/.outreach/.config/box/box.yaml"
 # download_box downloads the box configuration and
 # saves it to the box configuration file.
 download_box() {
-  local boxGitRepo="$(yq -r '.storageURL' "${HOMEBOX}")"
+  # Attempt to read the URL from env (this would be a CircleCI context or other setting)
+  # otherwise fall back to the stub that's created in circleci/setup.sh. Eventually, we'll
+  # want to only support the environment variable path.
+  local boxGitRepo="$BOX_REPOSITORY_URL"
+  if [[ -z "$boxGitRepo" ]] && [[ ! -e "$BOXPATH" ]]; then
+    echo "No box repository URL provided, and no box configuration stub found at $BOXPATH to infer it from" >&2
+    return 1
+  elif [[ -z "$boxGitRepo" ]]; then
+    boxGitRepo="$(yq -r '.storageURL' "$BOXPATH")"
+  fi
+
   local tempDir="$(mktemp -d)"
   trap 'rm -rf "${TMPDIR}"' EXIT
 
