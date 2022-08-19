f37547569649e49462ba64ccf2cb48aa90af55b0
Jared Allard <jared.allard@outreach.io>
2022-08-19 17:27:36 +0200

    fix ctx cancel issue

--- e2e/e2e.go
+++ e2e/e2e.go
@@ -264,7 +264,7 @@ func ensureRunningLocalizerWorks(ctx context.Context) error {
 
 	// not responding to pings, or failed to connect, remove the socket
 	//nolint:gosec // Why: We're OK with this. It's a constant.
-	return osStdInOutErr(exec.CommandContext(ctx, "sudo", "rm", "-f", localizer.Socket)).Run()
+	return osStdInOutErr(exec.Command("sudo", "rm", "-f", localizer.Socket)).Run()
 }
 
 func runLocalizer(ctx context.Context) (cleanup func(), err error) {
