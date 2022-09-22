3c498e4358b6b171dbd51b788b0c06b79af630a3
Jared Allard <jaredallard@outlook.com>
2022-09-21 20:24:30 -0700

    yarn_install_if_needed for prettier format

--- shell/linters/prettier.sh
+++ shell/linters/prettier.sh
@@ -6,10 +6,12 @@
 extensions=(yaml yml json md)
 
 prettier_linter() {
+  yarn_install_if_needed >/dev/null
   git ls-files '*.yaml' '*.yml' '*.json' '*.md' | xargs -n40 "node_modules/.bin/prettier" -l --loglevel warn
 }
 
 prettier_formatter() {
+  yarn_install_if_needed >/dev/null
   git ls-files '*.yaml' '*.yml' '*.json' '*.md' | xargs -n40 "node_modules/.bin/prettier" --write --loglevel warn
 }
 
