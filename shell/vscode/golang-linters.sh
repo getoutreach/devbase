0328e270b5ea5da36ff6100a73935063fa8ccc30
Jared Allard <jaredallard@outlook.com>
2022-09-22 19:32:00 -0700

    feat(vscode): lintroller

---
+++ shell/vscode/golang-linters.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# This script exists to run multiple linters for vscode acting
+# as a single one, to get the messages to show up in VSCode.
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+# forward signals to child processes
+trap 'kill $(jobs -p)' EXIT
+
+# run in background so we can forward signals
+"$DIR/../lintroller.sh" "$@" &
+
+# Wait for lintroller to finish
+wait
+
+exec "$DIR/../golangci-lint.sh" "$@"
