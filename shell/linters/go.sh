d962a40e101fb95911e5d0b6b20831fe3a028f0b
Jared Allard <jaredallard@users.noreply.github.com>
2022-09-28 10:57:27 -0700

    feat(vscode): lintroller (#317)
    * feat(vscode): lintroller
    
    * chmod +x
    
    * pr feedback

--- shell/linters/go.sh
+++ shell/linters/go.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 # Linters for Golang
-GOBIN="$DIR/gobin.sh"
-LINTER="${LINTER:-"$DIR/golangci-lint.sh"}"
 
 # Why: Used by the script that calls us
 # shellcheck disable=SC2034
 extensions=("go")
 
 lintroller() {
-  # The sed is used to strip the pwd from lintroller output,
-  # which is currently prefixed with it.
-  GOFLAGS=-tags=or_e2e,or_test "$GOBIN" \
-    "github.com/getoutreach/lintroller/cmd/lintroller@v$(get_application_version "lintroller")" \
-    -config scripts/golangci.yml ./... 2>&1 | sed "s#^$(pwd)/##"
+  "$DIR/lintroller.sh" ./...
 }
 
 go_mod_tidy() {
@@ -32,6 +26,6 @@ go_mod_tidy() {
 linter() {
   run_linter "go mod tidy" go_mod_tidy
   run_linter "golangci-lint" \
-    "$LINTER" --build-tags "or_e2e,or_test" --timeout 10m run ./...
+    "$DIR/golangci-lint.sh" --build-tags "or_e2e,or_test" --timeout 10m run ./...
   run_linter "lintroller" lintroller
 }
