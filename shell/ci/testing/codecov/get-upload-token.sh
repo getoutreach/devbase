92c7c6bd855468169a0f891b99342ebf47018fef
Jared Allard <jaredallard@outlook.com>
2022-09-27 20:40:22 -0700

    fixes

--- shell/ci/testing/codecov/get-upload-token.sh
+++ shell/ci/testing/codecov/get-upload-token.sh
@@ -21,18 +21,20 @@ fi
 owner="$1"
 repo="$2"
 
-info="$(curl -s -X GET "https://codecov.io/api/gh/$owner/$repo" -H "Authorization: $CODECOV_API_KEY")"
-
-# shellcheck disable=SC2181
-if [[ $? -ne 0 ]]; then
-  echo "Non-zero exit code returned from curl to codecov to get repository information: $info" >&2
+info="$(curl -s -o - -X GET -H "Authorization: $CODECOV_API_KEY" "https://codecov.io/api/gh/$owner/$repo")"
+if [[ -z $info ]]; then
+  echo "Failed to get repository information from Codecov." >&2
   exit 1
 fi
+active="$(jq -r '.repo.active' <<<"$info")"
+private="$(jq -r '.repo.private' <<<"$info")"
 
-# Check to see if the repository is not already active.
-if [[ $(jq '.repo.active | not' <<<"$info") == "true" ]]; then
-  echo "Attempting to activate repository." >&2
-  curl -s -X POST "https://codecov.io/api/pub/gh/$owner/$repo/settings" -d 'action=activate' -H "Authorization: $CODECOV_API_KEY"
+# Check to see if the repository is not already active, and if it is private.
+# If it's not private, we don't need to activate it.
+if [[ $active == "false" ]] && [[ $private == "true" ]]; then
+  echo "Activating repository $owner/$repo in codecov" >&2
+  curl -X POST -H "Authorization: $CODECOV_API_KEY" -o - \
+    "https://codecov.io/api/pub/gh/$owner/$repo/settings" -d 'action=activate' >&2
 fi
 
 upload_token="$(jq -r '.repo.upload_token' <<<"$info")"
@@ -43,8 +45,9 @@ fi
 # If we have a CirleCI API token, attempt to upload the upload token
 # to CircleCI
 if [[ $CIRCLECI == "true" ]] && [[ -n $CIRCLE_API_TOKEN ]]; then
-  curl -s -H "Content-Type: application/json" -H "Circle-Token: $CIRCLE_API_TOKEN" -H "Accept: application/json" \
-    -X POST "https://circleci.com/api/v2/project/github/$owner/$repo/envvar" \
+  curl -s -X POST \
+    -H "Content-Type: application/json" -H "Circle-Token: $CIRCLE_API_TOKEN" -H "Accept: application/json" \
+    "https://circleci.com/api/v2/project/github/$owner/$repo/envvar" \
     -d "{\"name\":\"CODECOV_UPLOAD_TOKEN\",\"value\":\"$upload_token\"}" || true
 fi
 
