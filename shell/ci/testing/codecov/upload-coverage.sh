086f4a73cff7e3416e71ed8fcbaf5964a1753372
Jared Allard <jaredallard@outlook.com>
2022-09-27 20:18:02 -0700

    clean up

--- shell/ci/testing/codecov/upload-coverage.sh
+++ shell/ci/testing/codecov/upload-coverage.sh
@@ -1,56 +1,56 @@
 #!/usr/bin/env bash
+# Uploads code coverage to codecov.io
+#
+# Note: This is not meant to be called outside of coverage.sh
+set -e
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
+# Ensure that we have a codecov API key.
 if [[ -z $CODECOV_API_KEY ]]; then
   echo "CODECOV_API_KEY environment variable is empty." >&2
   exit 1
 fi
 
-if [[ $CIRCLECI == "true" ]]; then
-  owner="${CIRCLE_PROJECT_USERNAME}"
-  repo="${CIRCLE_PR_REPONAME:-$CIRCLE_PROJECT_REPONAME}"
-else
-  if [[ $# -lt 2 ]]; then
-    echo "When running outside of CIRCLECI the first two positional arguments passed to this script need to be owner and repository." >&2
-    exit 1
-  fi
+coverage_file="$1"
+coverage_group="$2"
 
-  owner="$1"
-  repo="$2"
-  shift
-  shift
-fi
-
-upload_token="$("$DIR"/get-upload-token.sh "$owner" "$repo")"
-
-# shellcheck disable=SC2181
-if [[ $? -ne 0 ]]; then
-  echo "Failed to get upload token from codecov API." >&2
+origin="$(git remote get-url origin)"
+owner="$(sed -E 's/.*github.com[:\/]([^\/]+)\/([^\/]+).*/\1/' <<<"$origin")"
+repo="$(sed -E 's/.*github.com[:\/]([^\/]+)\/([^\/]+).*/\2/' <<<"$origin")"
+if [[ -z $owner || -z $repo ]]; then
+  echo "Could not determine owner and repo." >&2
   exit 1
 fi
 
-args=("-t" "$upload_token")
-if [[ $# -gt 0 ]]; then
-  args=("${args[@]}" "$@")
+upload_token="$("$DIR"/get-upload-token.sh "$owner" "$repo")"
+if [[ -z $upload_token ]]; then
+  echo "Failed to get coverage upload token" >&2
+  exit 1
 fi
 
-# Install codecov binary if it doesn't already exist in $PATH
-if [[ ! -x "$(command -v codecov)" ]]; then
+# Default arguments is the upload token and the coverage file
+args=("-t" "$upload_token" "-f" "$coverage_file")
+
+# If we have a coverage group, add it to the arguments
+if [[ -n $coverage_group ]]; then
+  args+=("-F" "$coverage_group")
+fi
+
+# If the codecov CLI is not found, download it into a temporary directory.
+codecovPath="$(command -v codecov)"
+if [[ ! -e $codecovPath ]] || [[ ! -x $codecovPath ]]; then
+  tempDir="$(mktemp -d)"
+  trap 'rm -rf $tempDir' EXIT
+
   os="linux"
   if [[ "$(uname)" == "Darwin" ]]; then
     os="macos"
   fi
 
-  curl -OsS "https://uploader.codecov.io/latest/$os/codecov"
-  chmod +x codecov
-  sudo mv codecov /usr/local/bin
+  curl -sS -o "$tempDir/codecov" "https://uploader.codecov.io/latest/$os/codecov"
+  chmod +x "$tempDir/codecov"
+  codecovPath="$tempDir/codecov"
 fi
 
-if ! codecov "${args[@]}"; then
-  echo "codecov upload failed." >&2
-  exit 1
-fi
-
-echo "Succesfully uploaded codecov report."
-exit 0
+exec "$codecovPath" "${args[@]}"
