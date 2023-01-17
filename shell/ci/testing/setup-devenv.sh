3598b4b423e133cad1e53d4d3ad238c4b4fad102
George Shaw <george.shaw@outreach.io>
2023-01-17 11:27:59 -0800

    remove bootstrap lib from shell script

--- shell/ci/testing/setup-devenv.sh
+++ shell/ci/testing/setup-devenv.sh
@@ -7,10 +7,6 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 # shellcheck source=../../lib/logging.sh
 source "$DIR/../../lib/logging.sh"
 
-# shellcheck source=../../lib/bootstrap.sh
-source "$DIR/../../lib/bootstrap.sh"
-PROJECT_DIR="$(get_repo_directory)"
-
 # Arguments
 PROVISION="${PROVISION:-"false"}"
 PROVISION_ARGS="${PROVISION_ARGS:-""}"
