f1ed94615ba2c8334b6c8b69ba5010abacd1f09f
Nate Kaldor <nate.kaldor@gmail.com>
2023-02-06 13:47:29 -0800

    fix: publish all files every release (#424)

--- shell/ci/release/docs.sh
+++ shell/ci/release/docs.sh
@@ -9,20 +9,18 @@ LIB_DIR="${DIR}/../../lib"
 source "${LIB_DIR}/logging.sh"
 
 TAG="$CIRCLE_TAG"
-if [[ -z $TAG ]]; then
-  # Calculate the psuedo-semver tag, this is used for non-v2 services
-  # (things without semantic-release, generally)
-  TAG="v0.0.0-$(TZ=UTC git --no-pager show --quiet --abbrev=12 --date='format-local:%Y%m%d%H%M%S' --format='%cd-%h')"
+# Do not update engdocs unless there is a tag
+if [[ -n $TAG ]]; then
+  # We need to use the module path to support major versions properly
+  MODULE_PATH="$(go list -f '{{ .Path }}' -m)"
+
+  # TODO(jaredallard): Move this into box configuration?
+  URL="https://engdocs.outreach.cloud/fetch/$MODULE_PATH@$TAG"
+
+  info "updating engdocs"
+  curl -X POST "$URL"
 fi
 
-# We need to use the module path to support major versions properly
-MODULE_PATH="$(go list -f '{{ .Path }}' -m)"
-
-# TODO(jaredallard): Move this into box configuration?
-URL="https://engdocs.outreach.cloud/fetch/$MODULE_PATH@$TAG"
-
-info "updating engdocs"
-curl -X POST "$URL"
-
+# Confluence docs get updated every run
 info "publishing eligible markdown documents to confluence"
-"$DIR/confluence-publish.sh"
+exec "$DIR/confluence-publish.sh"
