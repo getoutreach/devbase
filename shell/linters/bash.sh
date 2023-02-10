882b13022b4c282745d8f079e8892a4e307e2589
Antony Mbugua <tony.mbugua@outreach.io>
2023-02-09 16:20:59 -0800

    return 1

--- shell/linters/bash.sh
+++ shell/linters/bash.sh
@@ -20,8 +20,8 @@ shellfmt_formatter() {
 }
 
 linter() {
-  run_command "shellcheck" shellcheck_linter || exit 1
-  run_command "shellfmt" shellfmt_linter || exit 1
+  run_command "shellcheck" shellcheck_linter || return 1
+  run_command "shellfmt" shellfmt_linter || return 1
 }
 
 formatter() {
