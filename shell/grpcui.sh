471c19710d5a1c94ae7691f87e6e56feb7d7a8f8
Antony Mbugua <tony.mbugua@outreach.io>
2023-03-22 11:57:18 -0700

    fix: tonymbugua-DT-3582

--- shell/grpcui.sh
+++ shell/grpcui.sh
@@ -10,4 +10,13 @@ source "$DIR/lib/bootstrap.sh"
 # We set -plaintext here because we don't use GRPC TLS
 args=("-plaintext" "$@")
 
-exec "$GOBIN" "github.com/fullstorydev/grpcui/cmd/grpcui@v$(get_application_version "grpcui")" "${args[@]}"
+"$GOBIN" "github.com/fullstorydev/grpcui/cmd/grpcui@v$(get_application_version "grpcui")" "${args[@]}"
+
+# check if the grpcui command failed
+if [ $? -ne 0 ]; then
+    echo
+    echo "this expects your service to either be running locally or have a prot forward.
+to port forward:
+  - deploy to devenv (i.e. \"devenv app deploy .\")
+  - run \"kubectl port-forward service/[SERVICE-NAME] 5000:5000 -n [NAMESPACE]\""
+fi
