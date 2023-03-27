4bfe05ddf9af3bd916c359c4e8337ab9d7e8d986
Marek Fexa <marek.fexa@outreach.io>
2023-03-27 16:51:04 +0200

    Deploy deps in gorutines

--- e2e/e2e.go
+++ e2e/e2e.go
@@ -221,19 +221,27 @@ func provisionNew(ctx context.Context, target string) error { // nolint:unparam 
 
 // deployDeps deploys app dependencies
 func deployDeps(ctx context.Context, deps []string) {
-	for _, d := range deps {
-		// Skip applications that are already deployed, this is usually when
-		// they're in a snapshot we just provisioned from.
-		if appAlreadyDeployed(ctx, d) {
-			log.Info().Msgf("App %s already deployed, skipping", d)
-			continue
-		}
+	var wg sync.WaitGroup
 
-		log.Info().Msgf("Deploying dependency '%s'", d)
-		if err := osStdInOutErr(exec.CommandContext(ctx, "devenv", "--skip-update", "apps", "deploy", d)).Run(); err != nil {
-			log.Fatal().Err(err).Msgf("Failed to deploy dependency '%s'", d)
-		}
+	for _, dep := range deps {
+		wg.Add(1)
+
+		go func(wg *sync.WaitGroup, d string) {
+			defer wg.Done()
+			// Skip applications that are already deployed, this is usually when
+			// they're in a snapshot we just provisioned from.
+			if appAlreadyDeployed(ctx, d) {
+				log.Info().Msgf("App %s already deployed, skipping", d)
+				return
+			}
+
+			log.Info().Msgf("Deploying dependency '%s'", d)
+			if err := osStdInOutErr(exec.CommandContext(ctx, "devenv", "--skip-update", "apps", "deploy", d)).Run(); err != nil {
+				log.Fatal().Err(err).Msgf("Failed to deploy dependency '%s'", d)
+			}
+		}(&wg, dep)
 	}
+	wg.Wait()
 }
 
 // shouldRunE2ETests denotes whether or not this needs to actually
