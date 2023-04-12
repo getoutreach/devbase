3c991a2f1536bc0745f335086e87431a97477155
Jared Allard <jaredallard@users.noreply.github.com>
2023-04-12 09:23:33 -0700

    fix(debug): always use dlv debug, better docs (#510)
    * fix(debug): always use dlv debug, better docs
    
    * error when DLV_PORT isnt set but HEADLESS is true

--- shell/debug.sh
+++ shell/debug.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# debug.sh - Debug a Go application.
 set -e
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
@@ -6,46 +7,87 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 # shellcheck source=./lib/bootstrap.sh
 source "$DIR/lib/bootstrap.sh"
 
-export GOPROXY=https://proxy.golang.org
-export GOPRIVATE="github.com/getoutreach/*"
-export CGO_ENABLED=1
+# PACKAGE_TO_DEBUG is the package to debug. If not set, it will default to the
+# main package, which is cmd/<app_name>.
+PACKAGE_TO_DEBUG="${PACKAGE_TO_DEBUG:-$(get_repo_directory)/cmd/$(get_app_name)}"
 
-if [[ -z ${DLV_PORT} ]] && [[ -z $KUBERNETES_SERVICE_HOST ]]; then
-  exec "$SCRIPTS_DIR/gobin.sh" github.com/go-delve/delve/cmd/dlv@v"$(get_application_version "delve")" debug --build-flags="-tags=or_dev" "$(get_repo_directory)/cmd/$(get_app_name)"
-  exit
+# IN_CONTAINER is a flag that indicates whether or not we are running in a
+# container or not. If not set, will be determined automatically.
+#
+# Why: Declared for documentation/discoverability purposes.
+# shellcheck disable=SC2269
+IN_CONTAINER="${IN_CONTAINER}"
+
+# Determine if we're running in a container or not.
+if [[ -z $IN_CONTAINER ]]; then
+  # DLV_PORT is the port that DLV will listen on and is set by the devcontainer.
+  # KUBERNETES_SERVICE_HOST is set automatically when running in Kubernetes, which
+  # is a good indicator that we're running in a container.
+  if [[ -z ${DLV_PORT} ]] && [[ -z $KUBERNETES_SERVICE_HOST ]]; then
+    IN_CONTAINER=false
+  else
+    IN_CONTAINER=true
+  fi
 fi
 
+# HEADLESS is a flag that indicates whether or not we should run the debugger in
+# headless mode. If not set, it will be enabled if we're in a container. Otherwise,
+# it will be disabled.
+#
+# Running in headless mode starts delve in a mode where it will wait for a
+# debugger to connect before starting the application.
+HEADLESS="${HEADLESS:-$IN_CONTAINER}"
+
+# DLV_PORT is the port that DLV will listen on when running in headless mode.
+#
+# Why: Declared for documentation/discoverability purposes.
+# shellcheck disable=SC2269
+DLV_PORT="${DLV_PORT}"
+if [[ $HEADLESS == "true" ]] && [[ -z $DLV_PORT ]]; then
+  echo "DLV_PORT must be set when running in headless mode" >&2
+  exit 1
+fi
+DLV_VERSION="v$(get_application_version "delve")"
+
+# DEV_CONTAINER_LOGFILE is the path to the log file that will be used for all output
+# from the debugger. This is only used when running in a container and ideally would be
+# be used for propagating logs to a logging system.
+DEV_CONTAINER_LOGFILE=${DEV_CONTAINER_LOGFILE:-$TMPDIR/app.log}
+
+echo "Starting debugger for package '$PACKAGE_TO_DEBUG' (headless: $HEADLESS)" >&2
+if [[ $HEADLESS == "true" ]]; then
+  echo "Headless Information:" >&2
+  echo "  - DLV_PORT: $DLV_PORT" >&2
+  echo "  - DEV_CONTAINER_LOGFILE: $DEV_CONTAINER_LOGFILE" >&2
+
+  mkdir -p "$(dirname "$DEV_CONTAINER_LOGFILE")" 2>/dev/null >&2 || true
+fi
+echo
+
+delve_path=$("$DIR/gobin.sh" -p github.com/go-delve/delve/cmd/dlv@"$DLV_VERSION")
+
+# delve is the command that will be executed to start the debugger
 delve=(
-  "$DIR/gobin.sh"
-  github.com/go-delve/delve/cmd/dlv@v"$(get_application_version "delve")"
-  exec
-  "$(get_repo_directory)/bin/${DEV_CONTAINER_EXECUTABLE:-$(get_app_name)}"
-  --headless
-  --api-version=2
-  --listen=":${DLV_PORT}"
+  "$delve_path"
+  debug
+  --build-flags="-tags=or_dev"
+  "$PACKAGE_TO_DEBUG"
 )
 
-if [[ -z $DEVBOX_LOGFMT ]] && [[ -z $LOGFMT_FORMAT ]] && [[ -z $LOGFMT_FILTER ]]; then
-  exec "${delve[@]}" |
-    tee -ai "${DEV_CONTAINER_LOGFILE:-/tmp/app.log}"
-
-  exit
+# Set flags for the debugger when running in headless mode
+if [[ $HEADLESS == "true" ]]; then
+  delve+=(
+    --headless
+    --api-version=2
+    --listen=":${DLV_PORT}"
+  )
 fi
 
-logfmt=(
-  $"$DIR/gobin.sh"
-
-  "github.com/getoutreach/logfmt/cmd/logfmt@$(get_tool_version "getoutreach/logfmt")"
-)
-
-if [[ -n $LOGFMT_FORMAT ]]; then
-  logfmt+=(--format "$LOGFMT_FORMAT")
+# When not running in a container, we can start without logging.
+# Otherwise, we need to log to a file so that the output can be
+# processed by devspace.
+if [[ $IN_CONTAINER == "false" ]]; then
+  exec "${delve[@]}"
+else
+  exec "${delve[@]}" | tee -ai "$DEV_CONTAINER_LOGFILE"
 fi
-
-if [[ -n $LOGFMT_FILTER ]]; then
-  logfmt+=(--filter "$LOGFMT_FILTER")
-fi
-
-exec "${delve[@]}" |
-  tee -ai "${DEV_CONTAINER_LOGFILE:-/tmp/app.log}" |
-  "${logfmt[@]}"
