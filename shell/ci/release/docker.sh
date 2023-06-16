b003770168d5804d009b54f7f07d9641943d9566
Joey Smith <joey.smith@outreach.io>
2023-06-16 12:49:44 -0700

    fix: don't fail if the field is not an array

--- shell/ci/release/docker.sh
+++ shell/ci/release/docker.sh
@@ -34,7 +34,7 @@ get_image_field() {
   local name="$1"
   local field="$2"
   # shellcheck disable=SC1087 # this is a yq/jq filter that shellcheck thinks is bash
-  yq -r ".[\"$name\"]$field[]" "$MANIFEST"
+  yq -r ".[\"$name\"]$field[]?" "$MANIFEST"
 }
 
 # build_and_push_image builds and pushes a docker image to
