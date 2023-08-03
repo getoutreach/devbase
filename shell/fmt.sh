73bad98b95ede9898428831d7507bddbf1ca39be
David Boon <david.boon@outreach.io>
2023-08-03 15:13:41 -0700

    fix: add missing per-project formatter for migrations (#616)
    * fix: add missing per-project formatter for migrations
    
    adds per-project formatters, just like linters
    
    * make fmt

--- shell/fmt.sh
+++ shell/fmt.sh
@@ -13,11 +13,19 @@ source "$DIR/lib/bootstrap.sh"
 # shellcheck source=./lib/shell.sh
 source "$DIR/lib/shell.sh"
 
+# add extra (per project) linters
+linters=("$DIR/linters"/*.sh)
+if [[ -z $workspaceFolder ]]; then
+  workspaceFolder="$(get_repo_directory)"
+fi
+if [[ -d "$workspaceFolder"/scripts/linters ]]; then
+  linters+=("$workspaceFolder/scripts/linters/"*.sh)
+fi
+
 info "Running formatters"
 
 started_at="$(get_time_ms)"
-for languageScript in "$DIR/linters"/*.sh; do
-  languageName="$(basename "${languageScript%.sh}")"
+for linterScript in "${linters[@]}"; do
 
   # We use a sub-shell to prevent inheriting
   # the changes to functions/variables to the parent
@@ -29,7 +37,7 @@ for languageScript in "$DIR/linters"/*.sh; do
 
     # Why: Dynamic
     # shellcheck disable=SC1090
-    source "$DIR/linters/$languageName.sh"
+    source "$linterScript"
 
     matched=false
     if [[ "$(find_files_with_extensions "${extensions[@]}" | wc -l | tr -d ' ')" -gt 0 ]]; then
