4c4ff74f0fac68fed635f5af84095ee83fe182d8
Jared Allard <jaredallard@users.noreply.github.com>
2023-10-17 10:23:57 -0700

    Update shell/ci/auth/github_packages.sh
    Co-authored-by: Mark Lee <malept@users.noreply.github.com>

--- shell/ci/auth/github_packages.sh
+++ shell/ci/auth/github_packages.sh
@@ -55,6 +55,6 @@ if command -v npm >/dev/null 2>&1; then
   cat >>"$HOME/.npmrc" <<EOF
 
 //npm.pkg.github.com/:_authToken=$GITHUB_TOKEN
-@getoutreach:registry=https://npm.pkg.github.com
+@$ORG:registry=https://npm.pkg.github.com
 EOF
 fi
