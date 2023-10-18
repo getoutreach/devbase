d1d4859022ac7fb28267c661be7e649ac186951f
Mark Lee <mark.lee@outreach.io>
2023-10-18 11:30:00 -0700

    feat: allow CGO_ENABLED to be configurable for development

---
+++ shell/cgo-enabled.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# This is a small script to determine whether a service manifest has
+# explicitly enabled compiling with CGo, and print out the appropriate
+# value for the CGO_ENABLED environment variable. Defaults to disabled.
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+# shellcheck source=./lib/bootstrap.sh
+source "$DIR/lib/bootstrap.sh"
+
+if [[ "$(yq -r ".arguments.enableCGo" <"$(get_service_yaml)")" == "true" ]]; then
+  echo "1"
+else
+  echo "0"
+fi
