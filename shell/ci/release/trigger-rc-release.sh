a06209ccd56ac06c6baac527234001ad9fe4e649
Yujie Yan <yujie.yan@outreach.io>
2024-03-05 17:09:31 -0800

    fix: config release uer in service.yaml and default in box

--- shell/ci/release/trigger-rc-release.sh
+++ shell/ci/release/trigger-rc-release.sh
@@ -8,6 +8,9 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source "$DIR/../../lib/yaml.sh"
 # shellcheck source=./../../lib/bootstrap.sh
 source "$DIR/../../lib/bootstrap.sh"
+# shellcheck source=./../../lib/box.sh
+source "$DIR/../../lib/box.sh"
+
 
 if [[ "$(yaml_get_field ".arguments.releaseOptions.enablePrereleases" "$(get_service_yaml)")" != "true" ]]; then
   echo "releaseOptions.enablePrereleases is not true, skipping rc release"
@@ -17,11 +20,16 @@ fi
 # Default it to use main branch, should this be configurable?
 prereleaseBranch="main"
 
-# releaseUsername=$(get_box_field 'ci.circleci.username')
-# releaseUseremail=$(get_box_field 'ci.circleci.useremail')
+if [[ -n "$(yaml_get_field ".arguments.releaseOptions.releaseUser" "$(get_service_yaml)")" ]]; then
+  releaseUsername="$(yaml_get_field ".arguments.releaseOptions.releaseUser.name" "$(get_service_yaml)")"
+  releaseUseremail="$(yaml_get_field ".arguments.releaseOptions.releaseUser.email" "$(get_service_yaml)")"
+else
+  releaseUsername=$(get_box_field 'ci.circleci.username')
+  releaseUseremail=$(get_box_field 'ci.circleci.useremail')
+fi
 
-# git config --global user.name "$releaseUsername"
-# git config --global user.email "$releaseUseremail"
+git config --global user.name "$releaseUsername"
+git config --global user.email "$releaseUseremail"
 git checkout $prereleaseBranch
 
 # Dryrun the semantic-release on prereleaseBranch to check if there is changes to release.
