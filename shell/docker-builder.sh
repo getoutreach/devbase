3616b7a3236214dd984effd69d473ada1a480c78
--global <mark.lee@outreach.io>
2024-08-13 08:51:12 -0700

    fix: remove unused script

--- shell/docker-builder.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# DEPRECATED: Use below path instead.
-# Builds a docker image in CircleCI
-set -e
-
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-exec "$DIR/ci/release/docker.sh"
