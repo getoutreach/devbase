8d1bd325998624277eeea80de7529fe25b802926
Mark Lee <mark.lee@outreach.io>
2024-11-25 18:09:55 -0800

    refactor: authenticate docker auth based off of pull+push registry list

---
+++ shell/ci/release/docker-authn.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+#
+# Authenticate with supported Docker registries.
+# Resolves in the following prcedence order (empty values are ignored):
+# 1. From environment variables:
+#    a. DOCKER_PULL_REGISTRY | DOCKER_PUSH_REGISTRIES
+#    b. gobox-defined: BOX_DOCKER_PULL_IMAGE_REGISTRY | BOX_DOCKER_PUSH_IMAGE_REGISTRIES
+# 2. From box config:
+#    a. docker.imagePullRegistry | docker.imagePushRegistries
+#    b. devenv.imageRegistry
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+AUTH_DIR="${DIR}/../auth"
+LIB_DIR="${DIR}/../../lib"
+
+# shellcheck source=../../lib/logging.sh
+source "${LIB_DIR}/logging.sh"
+
+# shellcheck source=../../lib/docker.sh
+source "${LIB_DIR}/docker.sh"
+
+pullRegistry="${DOCKER_PULL_REGISTRY:-$(get_docker_pull_registry)}"
+pushRegistries="${DOCKER_PUSH_REGISTRIES:-$(get_docker_push_registries)}"
+
+registries=$(echo "$pullRegistry $pushRegistries" | tr ' ' '\n' | sort --unique | tr '\n' ' ')
+
+info "Authenticating with Docker registries"
+
+for crURL in $registries; do
+  case $crURL in
+  gcr.io/*)
+    info_sub "GCR"
+    # shellcheck source=../auth/gcr.sh
+    source "${AUTH_DIR}/gcr.sh"
+    ;;
+  *.amazonaws.com | *.amazonaws.com/*)
+    info_sub "AWS ECR"
+    # shellcheck source=../auth/aws-ecr.sh
+    source "${AUTH_DIR}/aws-ecr.sh"
+    ;;
+  *)
+    warn "No authentication script found for registry: $crURL"
+    ;;
+  esac
+done
