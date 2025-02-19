ffd1600d0224294cffd2b3b8ede768491417e3f7
Mark Lee <mark.lee@outreach.io>
2025-02-18 18:59:29 -0800

    Manually move mise to /usr/local/bin

--- shell/lib/mise.sh
+++ shell/lib/mise.sh
@@ -7,16 +7,15 @@ ensure_mise_installed() {
     gpg --keyserver hkps://keyserver.ubuntu.com --recv-keys 0x7413A06D
     curl https://mise.jdx.dev/install.sh.sig | gpg --decrypt >/tmp/mise-install.sh
     # ensure the above is signed with the mise release key
-    MISE_INSTALL_PATH=/usr/local/bin/mise sh /tmp/mise-install.sh
+    sh /tmp/mise-install.sh
+    sudo mv ~/.local/bin/mise /usr/local/bin/
 
     export MISE_OVERRIDE_TOOL_VERSIONS_FILENAMES=none
-    export PATH="$PATH:$HOME/.local/bin"
 
     # shellcheck disable=SC2016
     # Why: Appending a PATH to BASH_ENV
     {
       echo 'export MISE_OVERRIDE_TOOL_VERSIONS_FILENAMES=none'
-      echo 'export PATH="$PATH:$HOME/.local/bin"'
       echo 'eval "$(mise activate bash --shims)"'
     } >>"$BASH_ENV"
 
