0c977ee1baedd7eebc1df0fb858177b84244a64a
Mark Lee <malept@users.noreply.github.com>
2025-04-23 10:56:03 -0700

    fix(docs): handle repos with workspaces without crashing (#892)

--- shell/ci/release/docs.sh
+++ shell/ci/release/docs.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 # Trigger a private pkg.go.dev instance
+
 set -e
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 LIB_DIR="${DIR}/../../lib"
+
+# shellcheck source=../../lib/bootstrap.sh
+source "${LIB_DIR}/bootstrap.sh"
+
+# shellcheck source=../../lib/box.sh
+source "${LIB_DIR}/box.sh"
 
 # shellcheck source=../../lib/logging.sh
 source "${LIB_DIR}/logging.sh"
@@ -11,14 +18,21 @@ source "${LIB_DIR}/logging.sh"
 TAG="$CIRCLE_TAG"
 # Do not update engdocs unless there is a tag
 if [[ -n $TAG ]]; then
-  # We need to use the module path to support major versions properly
-  MODULE_PATH="$(go list -f '{{ .Path }}' -m)"
+  repo="$(get_box_field org)/$(get_app_name)"
+  # We need to use the module path to support major versions properly.
+  # Filter out module paths that aren't a part of this repo.
+  MODULE_PATHS="$(go list -f '{{ .Path }}' -m | grep "$repo")"
 
-  # TODO(jaredallard): Move this into box configuration?
-  URL="https://engdocs.outreach.cloud/fetch/$MODULE_PATH@$TAG"
+  BASE_URL="$(get_box_field engdocs.URL)"
 
   info "updating engdocs"
-  curl -X POST "$URL"
+  # There may be multiple module paths in a repo using workspaces.
+  for MODULE_PATH in $MODULE_PATHS; do
+    URL="$BASE_URL/fetch/$MODULE_PATH@$TAG"
+
+    info_sub "$MODULE_PATH@$TAG"
+    curl -X POST "$URL"
+  done
 fi
 
 # Confluence docs get updated every run
