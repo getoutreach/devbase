f14ffc0456b8f94e970591cc24a5e10035fa7d20
Mark Lee <mark.lee@outreach.io>
2025-06-13 18:34:28 -0700

    refactor(ruby/publish): use org from box config, fix up logging

--- shell/ruby/publish.sh
+++ shell/ruby/publish.sh
@@ -8,6 +8,8 @@ LIB_DIR="$SCRIPTS_DIR/lib"
 
 # shellcheck source=../lib/bootstrap.sh
 source "$LIB_DIR/bootstrap.sh"
+# shellcheck source=../lib/box.sh
+source "$LIB_DIR/box.sh"
 # shellcheck source=../lib/logging.sh
 source "$LIB_DIR/logging.sh"
 
@@ -16,8 +18,7 @@ clientDir="$(get_repo_directory)/api/clients/ruby"
 
 newVersion="$1"
 if [[ -z $newVersion ]]; then
-  error "Expected one argument, new version, but it was not provided"
-  exit 1
+  fatal "Expected one argument, new version, but it was not provided"
 fi
 
 DRYRUN_MODE=true
@@ -28,8 +29,7 @@ fi
 # replace -rc with .pre.rc which is what rake build does
 gemFile="$clientDir/pkg/${appName}_client-${newVersion//-rc/.pre.rc}.gem"
 if [[ ! -e $gemFile ]]; then
-  error "Error: gem file not found at '$gemFile', refusing to publish nothing"
-  exit 1
+  fatal ".gem file not found at '$gemFile', refusing to publish nothing"
 fi
 
 if [[ $DRYRUN_MODE == "true" ]]; then
@@ -39,8 +39,7 @@ fi
 
 pushd "$clientDir" >/dev/null || exit 1
 info "Pushing package to Github Packages"
-# TODO(jaredallard): Read the org from box when this is in CI.
 gem push --key github \
-  --host https://rubygems.pkg.github.com/getoutreach \
+  --host https://rubygems.pkg.github.com/"$(get_box_field org)" \
   "$gemFile"
 popd >/dev/null || exit 1
