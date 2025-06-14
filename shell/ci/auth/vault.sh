0c6301338910f73a9076b8bc4f88869800c14c84
Mark Lee <mark.lee@outreach.io>
2025-06-13 19:24:16 -0700

    refactor(vault): fetch address from box config

--- shell/ci/auth/vault.sh
+++ shell/ci/auth/vault.sh
@@ -2,9 +2,14 @@
 # Sets up vault authn
 set -e
 
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+DEVBASE_LIB_DIR="$DIR/../../lib"
+
+# shellcheck source=../../lib/box.sh
+source "$DEVBASE_LIB_DIR/box.sh"
+
 if [[ -n $VAULT_ROLE_ID ]] && [[ -n $VAULT_SECRET_ID ]]; then
-  # TODO(jaredallard): Put in box configuration. Need to build support for that.
-  VAULT_ADDR=https://vault-dev.outreach.cloud vault write auth/approle/login \
+  VAULT_ADDR="$(get_box_field devenv.vault.addressCI)" vault write auth/approle/login \
     role_id="$VAULT_ROLE_ID" secret_id="$VAULT_SECRET_ID" -format=json |
     jq .auth.client_token -r >"$HOME/.vault-token"
 else
