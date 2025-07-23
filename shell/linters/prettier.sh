2de3dc161b24f95ac58e1118855daf19eb96f0ba
Mark Lee <malept@users.noreply.github.com>
2025-07-23 08:18:12 -0700

    fix(prettier): typo in error message (#940)
    The tool in mise is `npm:prettier`.

--- shell/linters/prettier.sh
+++ shell/linters/prettier.sh
@@ -14,7 +14,7 @@ if [[ ! -f $PRETTIER && (! -f package.json || "$(gojq --raw-output .devDependenc
   # Try to find prettier installed via mise
   PRETTIER="$(mise which prettier)"
   if [[ -z $PRETTIER ]]; then
-    fatal "prettier not found in repo, make sure 'npx:prettier' is defined in 'mise.toml' and you have run 'mise install'"
+    fatal "prettier not found in repo, make sure 'npm:prettier' is defined in 'mise.toml' and you have run 'mise install'"
   fi
 fi
 
