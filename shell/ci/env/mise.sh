f4fdd3f6524a9b3c07508fb3d2ce776ecfc1bdd8
Mark Lee <malept@users.noreply.github.com>
2025-07-25 06:59:42 -0700

    fix(circleci): always run mise install if mise.toml exists in the repo (#944)

--- shell/ci/env/mise.sh
+++ shell/ci/env/mise.sh
@@ -3,5 +3,25 @@
 # or other CI platforms with that notion.
 set -e
 
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+LIB_DIR="${DIR}/../../lib"
+
+# shellcheck source=../../lib/bootstrap.sh
+source "${LIB_DIR}/bootstrap.sh"
+
+# shellcheck source=../../lib/logging.sh
+source "${LIB_DIR}/logging.sh"
+
+repo="$(get_repo_directory)"
+
 # TODO(malept): feature parity with asdf.sh in the same folder.
-mise install --yes
+if [[ -f "$repo"/mise.toml ]]; then
+  info_sub "🧑‍🍳 installing tool versions via mise"
+  if [[ -z $ALLOW_MISE_TO_MANAGE_TOOL_VERSIONS ]]; then
+    info_sub "🧑‍🍳 ignoring .tool-versions"
+    MISE_OVERRIDE_TOOL_VERSIONS_FILENAMES="none" mise install --cd "$repo" --yes
+  else
+    info_sub "🧑‍🍳 allowing mise to manage .tool-versions"
+    mise install --cd "$repo" --yes
+  fi
+fi
