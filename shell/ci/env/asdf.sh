86086d6697b02502f5d857242f57c07047346811
Stefan Sundin <28464633+stefanwork@users.noreply.github.com>
2025-08-26 18:45:25 -0700

    fix(ruby): remove ~/.default-gems generation (#950)
    ---------
    
    Co-authored-by: Mark Lee <malept@users.noreply.github.com>

--- shell/ci/env/asdf.sh
+++ shell/ci/env/asdf.sh
@@ -55,7 +55,6 @@ set_default_packages() {
   fi
   # language specifics
   echo -e "yarn" >"$HOME/.default-npm-packages"
-  echo -e "bundler 2.2.17" >"$HOME/.default-gems"
   cat >"$HOME/.default-golang-pkgs" <<EOF
 github.com/golang/protobuf/protoc-gen-go@v$protoc_go_version
 github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@v$protoc_doc_version
