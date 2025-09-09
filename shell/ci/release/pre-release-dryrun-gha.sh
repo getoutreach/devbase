c2b655844dfd7733c8608b04869bde0509d1b0a9
Mark Lee <mark.lee@outreach.io>
2025-09-09 15:12:24 -0700

    Fix typo

--- shell/ci/release/pre-release-dryrun-gha.sh
+++ shell/ci/release/pre-release-dryrun-gha.sh
@@ -19,7 +19,7 @@ source "$DIR/../../lib/yaml.sh"
 if [[ -n $CI ]]; then
   git config --global user.name "Devbase CI"
   git config --global user.email "devbase@outreach.io"
-  git auth status
+  gh auth status
   gh auth setup-git
   yarn install --frozen-lockfile
 fi
