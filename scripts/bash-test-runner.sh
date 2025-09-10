8ac5c3857d7506e1129e46d7809c90da5c6f1c07
Mark Lee <malept@users.noreply.github.com>
2025-09-10 16:49:23 -0700

    chore: explain why we're running `git submodule update --init`

--- scripts/bash-test-runner.sh
+++ scripts/bash-test-runner.sh
@@ -19,6 +19,7 @@ source "$DIR/../shell/lib/shell.sh"
 # shellcheck source=../shell/lib/bootstrap.sh
 source "$DIR/../shell/lib/bootstrap.sh"
 
+# Make sure that the bats-related submodules exist before running the bats tests.
 git submodule update --init
 
 # Find all files with _test.sh at the end of the filename
