8411ecd362aba3313a9c49f15980f193771a2bcf
Mark Lee <mark.lee@outreach.io>
2025-09-11 18:07:35 -0700

    Use ubi backend for uv

--- shell/ci/release/docker-authn.sh
+++ shell/ci/release/docker-authn.sh
@@ -48,7 +48,7 @@ ssh-add -l
 source "${AUTH_DIR}/ssh.sh"
 
 git config --global --remove-section url."ssh://git@github.com"
-RUST_BACKTRACE=full mise --verbose use --global uv
+RUST_BACKTRACE=full mise --verbose use --global ubi:astral-sh/uv
 mise exec yamllint -- yamllint ~/.config/gh/hosts.yml
 GH_NO_UPDATE_NOTIFIER=true gh auth setup-git
 
