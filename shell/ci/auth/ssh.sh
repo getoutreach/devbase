82a75c079297784448cc0262f4a798bd09907240
Mark Lee <mark.lee@outreach.io>
2025-09-11 17:33:56 -0700

    fix(ssh): only setup SSH access if ssh config exists

--- shell/ci/auth/ssh.sh
+++ shell/ci/auth/ssh.sh
@@ -2,8 +2,12 @@
 # Sets up SSH authentication in CI
 set -e
 
-# Setup SSH access
-ssh-add -D
+if [[ -f ~/.ssh/config ]]; then
+  # Setup SSH access
+  ssh-add -D
 
-# HACK: This is a fragile attempt to add whatever key is for github.com to our ssh-agent
-grep -A 2 github.com ~/.ssh/config | grep IdentityFile | awk '{ print $2 }' | xargs -n 1 ssh-add
+  # HACK: This is a fragile attempt to add whatever key is for github.com to our ssh-agent
+  grep -A 2 github.com ~/.ssh/config | grep IdentityFile | awk '{ print $2 }' | xargs -n 1 ssh-add
+else
+  warn
+fi
