c862d41e2194532c15b62fca26ece0b056bd2e04
Mark Lee <mark.lee@outreach.io>
2025-09-11 18:12:35 -0700

    Add message

--- shell/ci/auth/ssh.sh
+++ shell/ci/auth/ssh.sh
@@ -9,5 +9,5 @@ if [[ -f ~/.ssh/config ]]; then
   # HACK: This is a fragile attempt to add whatever key is for github.com to our ssh-agent
   grep -A 2 github.com ~/.ssh/config | grep IdentityFile | awk '{ print $2 }' | xargs -n 1 ssh-add
 else
-  warn
+  warn "No user SSH config, skipping setting up SSH access"
 fi
