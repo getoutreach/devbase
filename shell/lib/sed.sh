1d445902daa0b45281f3768362f14ac43b04eb5d
Mark Lee <mark.lee@outreach.io>
2025-09-19 22:35:54 -0700

    fix(sed): only support GNU sed

--- shell/lib/sed.sh
+++ shell/lib/sed.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #
 # Sed helper functions.
+# Assumes that logging.sh has been sourced already.
 
 # sed_in_place is a wrapper around sed -i that works on both Linux
 # and macOS.
@@ -8,14 +9,25 @@ sed_in_place() {
   local SED
   case "$OSTYPE" in
   darwin*)
-    SED="sed -i ''"
+    local gsed="${MACOS_GNU_SED:-gsed}"
+    if command -v "$gsed" >/dev/null; then
+      SED="$gsed"
+    else
+      read -r -d '' errMsg <<EOF
+macOS support requires GNU sed. Please install it via your developer setup
+tool (or via 'brew install gnu-sed' if there is no setup tool) and try the
+command again in a new terminal.
+EOF
+      error "$errMsg"
+      return 1
+    fi
     ;;
   linux*)
-    SED="sed -i"
+    SED="sed"
     ;;
   esac
 
-  $SED "$@"
+  $SED -i "$@"
 }
 
 # sed_replace is a wrapper around in-place sed replace that works on
