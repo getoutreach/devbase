bc21cc22ee56c3e83b8d433ab877ed6dd758219c
Egor Kalinichev <egor.kalinichev@outreach.io>
2025-11-06 16:45:18 +0100

    fix(lint): define local variable separately from assignment

--- shell/ci/release/docker-stitch.sh
+++ shell/ci/release/docker-stitch.sh
@@ -45,7 +45,8 @@ stitch_and_push_image() {
   local image="$1"
   local remoteImageNames=()
   for imageRegistry in $imageRegistries; do
-    local remoteImageName="$(determine_remote_image_name "$APPNAME" "$imageRegistry" "$image")"
+    local remoteImageName
+    remoteImageName="$(determine_remote_image_name "$APPNAME" "$imageRegistry" "$image")"
     remoteImageNames+=("$remoteImageName")
     if [[ $will_push == "true" ]] && [[ $remoteImageName =~ amazonaws.com($|/) ]]; then
       ensure_ecr_repository "$remoteImageName"
