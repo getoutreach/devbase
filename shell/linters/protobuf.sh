d9441877bd8cf5f10d7d87e95e50533454823ebd
Marek Nagy <marek.nagy@outreach.io>
2025-11-10 17:12:02 +0100

    add `buf lint` to `protobuf.sh`

--- shell/linters/protobuf.sh
+++ shell/linters/protobuf.sh
@@ -5,22 +5,34 @@
 # shellcheck disable=SC2034
 extensions=(proto)
 
+load_tool() {
+  "$DIR/gobin.sh" -p github.com/bufbuild/buf/cmd/buf@v"$(get_tool_version "buf")"
+}
+
 buf_linter() {
   # Why: We're OK with this.
   # shellcheck disable=SC2155
-  local PROTOFMT=$("$DIR/gobin.sh" -p github.com/bufbuild/buf/cmd/buf@v"$(get_tool_version "buf")")
+  local PROTOFMT=$(load_tool)
   find_files_with_extensions "${extensions[@]}" | xargs -n1 "$PROTOFMT" format --exit-code --diff
 }
 
 buf_formatter() {
   # Why: We're OK with this.
   # shellcheck disable=SC2155
-  local PROTOFMT=$("$DIR/gobin.sh" -p github.com/bufbuild/buf/cmd/buf@v"$(get_tool_version "buf")")
+  local PROTOFMT=$(load_tool)
   find_files_with_extensions "${extensions[@]}" | xargs -n1 "$PROTOFMT" format -w
+}
+
+buf_lint_linter() {
+  # Why: We're OK with this.
+  # shellcheck disable=SC2155
+  local PROTOFMT=$(load_tool)
+  find_files_with_extensions "${extensions[@]}" | xargs -n1 "$PROTOFMT" lint --path
 }
 
 linter() {
   run_command "buf" buf_linter
+  run_command "buf" buf_lint_linter
 }
 
 formatter() {
