8ee3ef08d8b8b93e10d75ea37eddcd05562fe102
Mark Lee <mark.lee@outreach.io>
2025-12-23 08:56:37 -0800

    chore: add helper to make mise-based stubs

---
+++ shell/lib/mise/stub.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# Helper for `mise`-based tool stubs.
+
+DIRNAME="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+DEVBASE_LIB_DIR="$DIRNAME/.."
+
+# shellcheck source=../bootstrap.sh
+source "$DEVBASE_LIB_DIR/bootstrap.sh"
+# shellcheck source=../logging.sh
+source "$DEVBASE_LIB_DIR/logging.sh"
+# shellcheck source=../mise.sh
+source "$DEVBASE_LIB_DIR/mise.sh"
+# shellcheck source=../shell.sh
+source "$DEVBASE_LIB_DIR/shell.sh"
