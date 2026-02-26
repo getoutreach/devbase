7d6d4cbf7f9179b189bd20617d7f0432eae38d9f
Mark Lee <mark.lee@outreach.io>
2026-02-25 18:07:00 -0800

    log to stderr

--- shell/lib/go.sh
+++ shell/lib/go.sh
@@ -7,7 +7,7 @@
 # directories) to skip specific directories.
 go_mod_dirs() {
   git ls-files --cached --others --modified --exclude-standard go.mod '**/go.mod' | xargs dirname | while read -r gomodDir; do
-    info "Ignored go.mod directories: ${IGNORED_GO_MOD_DIRS:-none}"
+    info "Ignored go.mod directories: ${IGNORED_GO_MOD_DIRS:-none}" >&2
     for ignored in ${IGNORED_GO_MOD_DIRS:-}; do
       if [[ $gomodDir == "$ignored" ]]; then
         continue 2
