4af1365f193dbc8c5f3cba77504af6f3fb90d9ba
Mark Lee <mark.lee@outreach.io>
2026-04-16 09:20:00 -0700

    Include mise lib

--- shell/ci/auth/vault.sh
+++ shell/ci/auth/vault.sh
@@ -8,6 +8,9 @@ DEVBASE_LIB_DIR="$DIR/../../lib"
 # shellcheck source=../../lib/box.sh
 source "$DEVBASE_LIB_DIR/box.sh"
 
+# shellcheck source=../../lib/mise.sh
+source "$DEVBASE_LIB_DIR/mise.sh"
+
 if [[ -n $VAULT_ROLE_ID ]] && [[ -n $VAULT_SECRET_ID ]]; then
   VAULT_ADDR="$(get_box_field devenv.vault.addressCI)" "$(find_tool vault)" write auth/approle/login \
     role_id="$VAULT_ROLE_ID" secret_id="$VAULT_SECRET_ID" -format=json |
